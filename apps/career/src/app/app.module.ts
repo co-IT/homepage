d3dd6aa7aab23458379528dc77fa2737393ed6a5
Gregor Woiwode <gregor.woiwode@gmail.com>
2022-05-19 12:27:02 +0200

    feat(career): setup i18n

--- apps/career/src/app/app.module.ts
+++ apps/career/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CareerI18nModule } from '@cp/career/i18n';
 import { CareerUiModule } from '@cp/career/ui';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    CareerUiModule.configure({
+    CareerI18nModule.forRoot(),
+    CareerUiModule.forRoot({
       browserTab: { titlePrefix: 'co-IT', titleSeparator: '-' },
     }),
     AppRoutingModule,
---
+++ libs/career/i18n/README.md
@@ -0,0 +1,7 @@
+# career-i18n
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test career-i18n` to execute the unit tests.
