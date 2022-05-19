e769b3bfc6c208352c2f0b6f220760a126eed7d2
Gregor Woiwode <gregor.woiwode@gmail.com>
2022-05-19 10:36:58 +0200

    feature(career-ui): provide page-title component

---
+++ libs/career/ui/src/lib/page-title/browser-tab-title-setter.service.ts
@@ -0,0 +1,29 @@
+import { Inject, Injectable } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import {
+  BrowserTabSettings,
+  BROWSER_TAB_SETTINGS,
+} from './browser-tab-title-settings.token';
+
+@Injectable()
+export class BrowserTabTitleSetter {
+  private separator: string;
+  private prefix: string;
+
+  constructor(
+    private title: Title,
+    @Inject(BROWSER_TAB_SETTINGS)
+    { titleSeparator, titlePrefix }: BrowserTabSettings
+  ) {
+    this.separator = titleSeparator;
+    this.prefix = titlePrefix;
+  }
+
+  set(newTitle: string): void {
+    const title = this.prefix
+      ? `${[this.prefix]} ${this.separator} ${newTitle}`
+      : newTitle;
+
+    this.title.setTitle(title);
+  }
+}
