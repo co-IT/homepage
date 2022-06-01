1ef7f0f9322cf1e17b4100a5c25d908930efe948
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2022-06-01 08:38:00 +0200

    feat(landing-page): display 3 texts

--- libs/ui/src/lib/text-in-box/text-in-box.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'cp-text-in-box',
-  templateUrl: './text-in-box.component.html',
-  styleUrls: ['./text-in-box.component.scss'],
-})
-export class TextInBoxComponent {}
