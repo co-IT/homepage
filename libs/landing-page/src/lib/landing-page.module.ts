9b9ebc9bdc8595557d579a126216e230bd9bdfce
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2022-06-01 08:08:55 +0200

    feat(landing-page): introduce box displaying text

--- libs/landing-page/src/lib/landing-page.module.ts
+++ libs/landing-page/src/lib/landing-page.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CareerUiModule } from '@cp/career/ui';
+import { LandingPageComponent } from './landing-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    CareerUiModule,
     RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+      { path: '', pathMatch: 'full', component: LandingPageComponent },
     ]),
   ],
+  declarations: [LandingPageComponent],
 })
 export class LandingPageModule {}
