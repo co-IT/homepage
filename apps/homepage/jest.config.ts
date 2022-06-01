6778c7bd707057c64f4897b86e08929a850d8528
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2022-06-01 07:52:18 +0200

    chore: move career to homepage

--- apps/homepage/jest.config.ts
+++ apps/homepage/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'career',
+  displayName: 'homepage',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/career',
+  coverageDirectory: '../../coverage/apps/homepage',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
