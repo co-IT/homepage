1b6582429e2b0e8c8172c5bde2422c5027e51720
Gregor Woiwode <gregor.woiwode@gmail.com>
2022-06-15 21:06:18 +0200

    feat(ui): expose article component

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/ui.module';
 export * from './lib/page-title/page-title.component';
 
 export * from './lib/timeline/timeline.component';
+
+export * from './lib/article/article.component';
