9ea67098ba9f02f0e8d30184eda65cebba82c52d
Gregor Woiwode <gregor.woiwode@gmail.com>
2022-06-15 21:48:35 +0200

    feat(career): support rendering markdown articles

--- libs/career/recruiting-process/src/lib/career-recruiting-process.component.ts
+++ libs/career/recruiting-process/src/lib/career-recruiting-process.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { PageTitleComponent } from '@cp/career/ui';
+import { ArticleComponent, PageTitleComponent } from '@cp/career/ui';
 import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'cp-career-recruiting-process',
   standalone: true,
-  imports: [TranslocoModule, PageTitleComponent],
+  imports: [TranslocoModule, PageTitleComponent, ArticleComponent],
   templateUrl: './career-recruiting-process.component.html',
   styleUrls: ['./career-recruiting-process.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
