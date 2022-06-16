15210b9b00145912950944e78bf12d60ec858820
Gregor Woiwode <gregor.woiwode@gmail.com>
2022-06-16 22:09:02 +0200

    feat(app): setup global navigation

--- apps/homepage/src/app/app.component.ts
+++ apps/homepage/src/app/app.component.ts
@@ -1,11 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {
+  HeaderComponent,
+  PageTitleComponent,
+  RouteInternal,
+  ToolbarComponent,
+} from '@cp/ui';
+import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'cp-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    TranslocoModule,
+    HeaderComponent,
+    PageTitleComponent,
+    ToolbarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  protected routeRecentlyClicked?: RouteInternal;
+
+  protected routesInternal: RouteInternal[] = [
+    {
+      path: '/welcome',
+      text: this.transloco.translate('navigation.welcome'),
+      title: 'landing-page.page-title',
+      subtitle: 'landing-page.page-subtitle',
+      isStartPage: true,
+    },
+    {
+      path: '/recruiting-process',
+      text: this.transloco.translate('navigation.career'),
+      title: 'recruiting-process.page-title',
+      subtitle: 'recruiting-process.page-subtitle',
+    },
+  ];
+
+  constructor(private transloco: TranslocoService) {}
+}
--- libs/ui/src/lib/toolbar/routes-internal.ts
+++ libs/ui/src/lib/toolbar/routes-internal.ts
@@ -1,4 +1,26 @@
 export interface RouteInternal {
+  /**
+   * Represents client path to page
+   */
   path: string;
+
+  /*
+   * Represents text of the link going to be rendered
+   */
   text: string;
+
+  /**
+   * Represents the title of the page belonging to the path
+   */
+  title: string;
+
+  /**
+   * Represents the subtitle of the page belonging to the path
+   */
+  subtitle: string;
+
+  /**
+   * Indicates whether route is entry point of the app.
+   */
+  isStartPage?: boolean;
 }
