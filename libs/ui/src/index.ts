917000a3609f33254eb8e07327f35fc6d4e2665f
Gregor Woiwode <gregor.woiwode@gmail.com>
2022-06-16 21:43:56 +0200

    feat(landing-page): setup toolbar

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/article/article.component';
 export * from './lib/configure-career-ui.provider';
+export * from './lib/header/header.component';
 export * from './lib/page-title/page-title.component';
 export * from './lib/timeline/timeline.component';
-
-export * from './lib/header/header.component';
+export * from './lib/toolbar/routes-internal';
+export * from './lib/toolbar/toolbar.component';
