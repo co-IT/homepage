47f12113c589527c05d55b1e4805764b44bafa46
Gregor Woiwode <gregor.woiwode@gmail.com>
2022-06-17 00:16:23 +0200

    feat(career): introduce video tiles

---
+++ libs/ui/src/lib/video-play-button/video-play-button.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cp-video-play-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './video-play-button.component.svg',
+  styleUrls: ['./video-play-button.component.scss'],
+})
+export class VideoPlayButtonComponent {}
