a3cb6598ef92f36a188d3aa72beb0bc63c52c667
Frank Strieter <fs@co-it.eu>
2022-06-22 16:03:56 +0200

    feat: [WIP] use yt playermodule to view videos

--- libs/career/recruiting-process/src/lib/cinema/cinema.component.ts
+++ libs/career/recruiting-process/src/lib/cinema/cinema.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { map, Observable, of } from 'rxjs';
+import { YouTubePlayerModule } from '@angular/youtube-player';
+import { TranslocoModule } from '@ngneat/transloco';
+import { ArticleComponent } from '@cp/ui';
 
 @Component({
   selector: 'cp-cinema',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    YouTubePlayerModule,
+    TranslocoModule,
+    ArticleComponent,
+  ],
   templateUrl: './cinema.component.html',
   styleUrls: ['./cinema.component.scss'],
 })
 export class CinemaComponent implements OnInit {
-  constructor() {}
+  videoId$: Observable<string> = of('');
+  apiLoaded = false;
+  constructor(activatedRoute: ActivatedRoute) {
+    this.videoId$ = activatedRoute.paramMap.pipe(
+      map((params: ParamMap) => params.get('videoId') || '')
+    );
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (!this.apiLoaded) {
+      // This code loads the IFrame Player API code asynchronously, according to the instructions at
+      // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      document.body.appendChild(tag);
+      this.apiLoaded = true;
+    }
+  }
 }
