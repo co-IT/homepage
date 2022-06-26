aaba7f6f460d5bff2d96eebfcba812d9cc74aa87
Frank Strieter <fs@co-it.eu>
2022-06-26 20:37:14 +0200

    feat(toolbar): display title even when navigating deeper

--- libs/career/recruiting-process/src/lib/career-recruiting-process.routes.ts
+++ libs/career/recruiting-process/src/lib/career-recruiting-process.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { CareerRecruitingProcessComponent } from './career-recruiting-process.component';
+import { CinemaComponent } from './cinema/cinema.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: CareerRecruitingProcessComponent,
     pathMatch: 'full',
+  },
+  {
+    path: 'cinema/:videoId',
+    component: CinemaComponent,
   },
 ];
