9bd0445d4f6ffc6cffdfda3d2bc9529466e1426e
Frank Strieter <fs@co-it.eu>
2022-06-26 21:39:34 +0200

    refactor: code review

--- libs/career/recruiting-process/src/lib/cinema-header/cinema-header.component.ts
+++ libs/career/recruiting-process/src/lib/cinema-header/cinema-header.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BackButtonComponent } from '@cp/ui';
 
 @Component({
   selector: 'cp-cinema-header',
   standalone: true,
-  imports: [CommonModule, BackButtonComponent],
+  imports: [CommonModule, BackButtonComponent, RouterModule],
   templateUrl: './cinema-header.component.html',
   styleUrls: ['./cinema-header.component.scss'],
 })
