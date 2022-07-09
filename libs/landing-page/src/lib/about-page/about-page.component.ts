b623f0126d12cee10890216eac6f2543e60a0635
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2022-07-09 11:14:06 +0200

    feat(about-page): update content & layout

--- libs/landing-page/src/lib/about-page/about-page.component.ts
+++ libs/landing-page/src/lib/about-page/about-page.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslocoModule } from '@ngneat/transloco';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'cp-about-page',
   standalone: true,
-  imports: [CommonModule, TranslocoModule],
+  imports: [CommonModule, TranslocoModule, MarkdownModule],
   templateUrl: './about-page.component.html',
   styleUrls: ['./about-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
