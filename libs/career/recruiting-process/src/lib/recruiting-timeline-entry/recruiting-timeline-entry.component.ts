13cadfc394e7859604deacf6929f1b1bfb4216f4
Gregor Woiwode <gregor.woiwode@gmail.com>
2022-07-10 13:03:52 +0200

    refactor(timeline-entry): introduce interface

--- libs/career/recruiting-process/src/lib/recruiting-timeline-entry/recruiting-timeline-entry.component.ts
+++ libs/career/recruiting-process/src/lib/recruiting-timeline-entry/recruiting-timeline-entry.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { TranslocoModule } from '@ngneat/transloco';
+import { TimelineEntry } from '../recruiting-timeline/time-line-entry.model';
 
 @Component({
   selector: 'cp-recruiting-timeline-entry',
@@ -10,5 +11,5 @@ import { TranslocoModule } from '@ngneat/transloco';
   styleUrls: ['./recruiting-timeline-entry.component.scss'],
 })
 export class RecruitingTimelineEntryComponent {
-  @Input() entry: any;
+  @Input() entry?: TimelineEntry;
 }
