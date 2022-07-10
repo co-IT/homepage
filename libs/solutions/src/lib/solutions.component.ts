c5ebea86c4f83633bafacf3397c26b258a77fbb7
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2022-07-10 21:48:37 +0200

    feat(solutions): implement solutions page

--- libs/solutions/src/lib/solutions.component.ts
+++ libs/solutions/src/lib/solutions.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MarkdownModule } from 'ngx-markdown';
+import { TranslocoModule } from '@ngneat/transloco';
+import { YouTubePlayerModule } from '@angular/youtube-player';
 
 @Component({
   selector: 'cp-solutions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, YouTubePlayerModule, MarkdownModule, TranslocoModule],
   templateUrl: './solutions.component.html',
   styleUrls: ['./solutions.component.scss'],
 })
 export class SolutionsComponent implements OnInit {
-  constructor() {}
+  private apiLoaded = false;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (!this.apiLoaded) {
+      // This code loads the IFrame Player API code asynchronously, according to the instructions at
+      // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      document.body.appendChild(tag);
+      this.apiLoaded = true;
+    }
+  }
 }
