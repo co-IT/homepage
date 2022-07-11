c5abbd0be4f84ae50fc1956dc7ccd9d47b314698
Frank Strieter <fs@co-it.eu>
2022-07-11 07:13:31 +0200

    feat: add burger menu

--- libs/ui/src/lib/header/header.component.ts
+++ libs/ui/src/lib/header/header.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationComponent } from '../navigation/navigation.component';
 
 @Component({
   selector: 'cp-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavigationComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
