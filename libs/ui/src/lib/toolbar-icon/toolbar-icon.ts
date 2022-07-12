677b5c25bd004e780327e25f78bbe375ecb3fd3c
Frank Strieter <fs@co-it.eu>
2022-07-12 10:30:24 +0200

    fix: styling according figma

--- libs/ui/src/lib/toolbar-icon/toolbar-icon.ts
+++
@@ -1,6 +0,0 @@
-export interface ToolbarIcon {
-  type?: string;
-  src?: string;
-  target?: '_blank';
-  text?: string;
-}
--- libs/ui/src/lib/toolbar/toolbar.component.ts
+++ libs/ui/src/lib/toolbar/toolbar.component.ts
@@ -3,8 +3,8 @@ import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslocoModule } from '@ngneat/transloco';
 import { NavigationButtonComponent } from '../navigation-button/navigation-button.component';
-import { ToolbarIcon } from '../toolbar-icon/toolbar-icon';
-import { ToolbarIconComponent } from '../toolbar-icon/toolbar-icon.component';
+import { ToolbarIconLink } from '../toolbar-icon/toolbar-icon-link';
+import { ToolbarIconLinkComponent } from '../toolbar-icon/toolbar-icon-link.component';
 
 @Component({
   selector: 'cp-toolbar',
@@ -14,7 +14,7 @@ import { ToolbarIconComponent } from '../toolbar-icon/toolbar-icon.component';
     RouterModule,
     TranslocoModule,
     NavigationButtonComponent,
-    ToolbarIconComponent,
+    ToolbarIconLinkComponent,
   ],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
@@ -23,19 +23,19 @@ export class ToolbarComponent {
   @Input() logoSrc?: string;
   @Input() logoAlt?: string;
 
-  toolbarIcons: ToolbarIcon[] = [
+  toolbarIconLinks: ToolbarIconLink[] = [
     {
-      type: 'phone',
-      src: 'tel:+4921935163052',
+      icon: 'mdi-phone',
+      href: 'tel:+4921935163052',
     },
     {
-      type: 'maps',
-      src: 'https://www.google.com/maps/place/co-IT.eu+GmbH/@49.005146,8.39661,15z/data=!4m5!3m4!1s0x0:0x2ccc0a1e2f9cab01!8m2!3d49.005146!4d8.39661',
+      icon: 'mdi-email-outline',
+      href: 'mailto:info@co-IT.eu',
+    },
+    {
+      icon: 'mdi-map',
+      href: 'https://www.google.com/maps/place/co-IT.eu+GmbH/@49.005146,8.39661,15z/data=!4m5!3m4!1s0x0:0x2ccc0a1e2f9cab01!8m2!3d49.005146!4d8.39661',
       target: '_blank',
-    },
-    {
-      type: 'mail',
-      src: 'mailto:info@co-IT.eu',
     },
   ];
 }
