74a3f06d511dfe00edac96a2ed788c7e0072a2b1
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2022-07-12 16:57:51 +0200

    feat(navigation): add hotkey to open/close menu

--- libs/ui/src/lib/navigation-button/navigation-button.component.ts
+++ libs/ui/src/lib/navigation-button/navigation-button.component.ts
@@ -1,28 +1,45 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { HotkeysModule, HotkeysService } from '@ngneat/hotkeys';
 import { TranslocoModule } from '@ngneat/transloco';
 import { tap } from 'rxjs/operators';
 import { NavigationService } from '../navigation/navigation.service';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
   selector: 'cp-navigation-button',
   templateUrl: './navigation-button.component.html',
   standalone: true,
-  imports: [CommonModule, TranslocoModule],
+  imports: [CommonModule, TranslocoModule, HotkeysModule],
   styleUrls: ['./navigation-button.component.scss'],
 })
 export class NavigationButtonComponent implements OnInit {
   isOpen = false;
 
-  constructor(private navigationService: NavigationService) {}
+  constructor(
+    private navigationService: NavigationService,
+    private hotkeys: HotkeysService
+  ) {}
 
   ngOnInit() {
     this.navigationService.isOpen$
-      .pipe(tap((isOpen) => (this.isOpen = isOpen)))
+      .pipe(
+        tap((isOpen) => (this.isOpen = isOpen)),
+        untilDestroyed(this)
+      )
+      .subscribe();
+
+    this.hotkeys
+      .addShortcut({ keys: 'meta.k' })
+      .pipe(
+        tap(() => this.toggleNavigation()),
+        untilDestroyed(this)
+      )
       .subscribe();
   }
 
-  toggleNavigation(isOpen: boolean) {
-    this.navigationService.setNavigationState(isOpen);
+  toggleNavigation() {
+    this.navigationService.setNavigationState(!this.isOpen);
   }
 }
