be1d2196ec22b6083c42f06808e2dfe1eba2d26d
Frank Strieter <fs@co-it.eu>
2022-07-12 10:58:37 +0200

    fix: contact routing

--- libs/ui/src/lib/call-to-action/call-to-action.component.ts
+++ libs/ui/src/lib/call-to-action/call-to-action.component.ts
@@ -1,15 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'cp-call-to-action',
   standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './call-to-action.component.html',
   styleUrls: ['./call-to-action.component.scss'],
 })
 export class CallToActionComponent {
   @Input() icon?: string;
   @Input() text?: string;
-  @Input() href?: string;
+  @Input() href?: string | null = null;
+  @Input() internalRouterLink: string[] | null = null;
   @Input() fullWidth = false;
   @Input() target?: string = '_blank';
 }
