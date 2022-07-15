488aebe1f1e69ddad21ce1bda8d44e9597dac522
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2022-07-15 13:25:44 +0200

    fix(career): rename translation keys

--- libs/sites/src/lib/career-page/career-video-repository.service.ts
+++ libs/sites/src/lib/career-page/career-video-repository.service.ts
@@ -5,103 +5,103 @@ import { VideoCollectionGrouped } from './models';
 export class CareerVideoRepository {
   videos: VideoCollectionGrouped = {
     why: {
-      heading: 'recruiting-process.video.why.heading',
+      heading: 'career.video.why.heading',
       appearance: 'blue',
       videos: [
         {
-          title: 'recruiting-process.video.why.founding',
+          title: 'career.video.why.founding',
           id: 'IZIduzPH9oU',
           icon: 'building',
         },
         {
-          title: 'recruiting-process.video.why.values',
+          title: 'career.video.why.values',
           id: 'ChBHRdP2WNE',
           icon: 'lifebelt',
         },
         {
-          title: 'recruiting-process.video.why.orientation',
+          title: 'career.video.why.orientation',
           id: 'n26k-IpzZAY',
           icon: 'woman-man',
         },
         {
-          title: 'recruiting-process.video.why.solutions',
+          title: 'career.video.why.solutions',
           id: '7trxOOr9Oys',
           icon: 'signpost',
         },
         {
-          title: 'recruiting-process.video.why.change',
+          title: 'career.video.why.change',
           id: 'v9KlP83Y73I',
           icon: 'compass',
         },
         {
-          title: 'recruiting-process.video.why.understand-people',
+          title: 'career.video.why.understand-people',
           id: 'kQBu_XUbRpQ',
           icon: 'hand-holds-diamond',
         },
         {
-          title: 'recruiting-process.video.why.origin',
+          title: 'career.video.why.origin',
           id: 'DE65AUyFI6E',
           icon: 'family',
         },
       ],
     },
     how: {
-      heading: 'recruiting-process.video.how.heading',
+      heading: 'career.video.how.heading',
       appearance: 'green',
       videos: [
         {
-          title: 'recruiting-process.video.how.your-development',
+          title: 'career.video.how.your-development',
           id: 'ES6SMX18Zgs',
           icon: 'building',
         },
         {
-          title: 'recruiting-process.video.how.comrades',
+          title: 'career.video.how.comrades',
           id: 'eeRmNqLyhZM',
           icon: 'hand-holds-diamond',
         },
         {
-          title: 'recruiting-process.video.how.higher-goal',
+          title: 'career.video.how.higher-goal',
           id: 'HkKkJ-rBG0I',
           icon: 'compass',
         },
         {
-          title: 'recruiting-process.video.how.leadership',
+          title: 'career.video.how.leadership',
           id: 'k2TDf0F3KI0',
           icon: 'signpost',
         },
         {
-          title: 'recruiting-process.video.how.customer-orientation',
+          title: 'career.video.how.customer-orientation',
           id: 'iy5X7MVtIoI',
           icon: 'family',
         },
         {
-          title: 'recruiting-process.video.how.networking',
+          title: 'career.video.how.networking',
           id: 'pqDwg2ny4fY',
           icon: 'woman-man',
         },
       ],
     },
     what: {
-      heading: 'recruiting-process.video.what.heading',
+      heading: 'career.video.what.heading',
       appearance: 'orange',
       videos: [
         {
-          title: 'recruiting-process.video.what.salary-1',
+          title: 'career.video.what.salary-1',
           id: '2AceUlURmwY',
           icon: 'hand-holds-diamond',
         },
         {
-          title: 'recruiting-process.video.what.salary-2',
+          title: 'career.video.what.salary-2',
           id: 'klWORYu1AKI',
           icon: 'hand-holds-diamond',
         },
         {
-          title: 'recruiting-process.video.what.salary-3',
+          title: 'career.video.what.salary-3',
           id: 'MOEhmbz-1UY',
           icon: 'hand-holds-diamond',
         },
         {
-          title: 'recruiting-process.video.what.contract',
+          title: 'career.video.what.contract',
           id: '2AceUlURmwY',
           icon: 'compass',
         },
