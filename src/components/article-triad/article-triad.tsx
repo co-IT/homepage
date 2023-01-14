0c3bb4faf46c9a5b25e70e9aeeb96f3d922e134c
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-01-14 23:23:45 +0100

    feat(article-triad): allow better centering

--- src/components/article-triad/article-triad.tsx
+++ src/components/article-triad/article-triad.tsx
@@ -14,13 +14,13 @@ import { component$, Slot } from '@builder.io/qwik';
 export const ArticleTriad = component$(() => {
   return (
     <article class="grid auto-rows-min md:grid-cols-8 md:grid-rows-1">
-      <div class="z-20 row-start-3 -translate-y-28 mx-3 pt-10 pb-10 pr-5 pl-5 grid-flow-row md:-translate-y-0 md:row-span-full md:col-start-1 md:col-span-4 md:p-14 shadow-md  self-center bg-white">
+      <div class="z-20 row-start-3 -translate-y-28 mx-3 pt-10 pb-10 pr-5 pl-5 grid-flow-row md:-translate-y-0 md:row-span-full md:col-start-2 md:col-span-4 md:p-14 shadow-md  self-center bg-white">
         <Slot name="article"></Slot>
       </div>
-      <div class="z-10 row-start-1 md:row-span-full md:col-start-4 md:col-span-4">
+      <div class="z-10 row-start-1 md:row-span-full md:col-start-5 md:col-span-3">
         <Slot name="image"></Slot>
       </div>
-      <div class="z-0 row-start-2 md:row-span-full md:col-start-7 xl:col-start-6 md:self-end md:translate-y-10 h-4 md:w-48 md:h-[32rem] xl:h-[40rem] bg-primary"></div>
+      <div class="z-0 row-start-2 md:row-span-full md:col-start-7 xl:col-start-7 md:self-end md:translate-y-10 h-4 md:w-48 md:h-[32rem] xl:h-[40rem] bg-primary"></div>
     </article>
   );
 });
