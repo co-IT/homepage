e356adf43686ed19900ff2f91fc2f2407c4acaf8
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-01-14 22:17:05 +0100

    feat(article-triad): improve overlapping

--- src/components/article-triad/article-triad.tsx
+++ src/components/article-triad/article-triad.tsx
@@ -1,15 +1,26 @@
 import { component$, Slot } from '@builder.io/qwik';
 
+/**
+ * Consists of three elements
+ * 1. A textbox overlapping...
+ * 2. ...An image being overlapped by...
+ * 3. ... a coloured border
+ *
+ * Overlapping is solved using a CSS Grid having 9 columns.
+ * The textbox stretches from column 1 to column 4.
+ * The image starts at column 4 goes to column 9.
+ * The border starts at column 8
+ */
 export const ArticleTriad = component$(() => {
   return (
-    <article class="grid grid-cols-9">
+    <article class="grid grid-cols-8">
       <div class="z-20 row-span-full col-start-1 col-span-4 p-14 shadow-md  self-center bg-white">
         <Slot name="article"></Slot>
       </div>
-      <div class="z-10 row-span-full col-start-4 col-span-5 ">
+      <div class="z-10 row-span-full col-start-4 col-span-4 ">
         <Slot name="image"></Slot>
       </div>
-      <div class="z-0 row-span-full col-end-8 self-end  w-48 h-96 bg-primary"></div>
+      <div class="z-0 row-span-full col-start-7 xl:col-start-6  self-end translate-y-10 w-48 md:h-[32rem] h-[40rem] bg-primary"></div>
     </article>
   );
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,22 +6,24 @@ import { LinkCallToAction } from '../components/link-call-to-action/link-call-to
 
 export default component$(() => {
   return (
-    <ArticleTriad>
-      <section q:slot="article">
-        <Heading>
-          <span q:slot="primary">Denken Sie auch in</span>
-          <span q:slot="accent">ganzheitlichen Lösungen?</span>
-        </Heading>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
-          repellat eveniet atque incidunt, praesentium quia sequi quidem dolorem
-          amet quos eos ex voluptatibus blanditiis magni reprehenderit corporis
-          maxime inventore laboriosam.
-        </p>
-        <LinkCallToAction href="/">Mehr erfahren</LinkCallToAction>
-      </section>
-      <img q:slot="image" src="/img/landing-page/developers-tablet.jpg"></img>
-    </ArticleTriad>
+    <div class="container mx-auto">
+      <ArticleTriad>
+        <section q:slot="article">
+          <Heading>
+            <span q:slot="primary">Denken Sie auch in</span>
+            <span q:slot="accent">ganzheitlichen Lösungen?</span>
+          </Heading>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
+            repellat eveniet atque incidunt, praesentium quia sequi quidem
+            dolorem amet quos eos ex voluptatibus blanditiis magni reprehenderit
+            corporis maxime inventore laboriosam.
+          </p>
+          <LinkCallToAction href="/">Mehr erfahren</LinkCallToAction>
+        </section>
+        <img q:slot="image" src="/img/landing-page/developers-tablet.jpg"></img>
+      </ArticleTriad>
+    </div>
   );
 });
 
