c988ca33bfcf8a52d70b31499115c4a0e9ce23ff
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-01-17 21:40:11 +0100

    feat(landing-page): introduce content-overlap components

---
+++ src/components/content-overlap/content-overlap-landscape.tsx
@@ -0,0 +1,35 @@
+import { component$, Slot } from '@builder.io/qwik';
+
+interface ContentOverlapLandscapeProps {
+  image: {
+    source: string;
+    alternateText: string;
+  };
+}
+
+/**
+ * Consists of three elements
+ * 1. A textbox overlapping...
+ * 2. ...An image being overlapped by...
+ * 3. ... a coloured border
+ *
+ * Overlapping is solved using a CSS Grid having 9 columns.
+ * The textbox stretches from column 1 to column 4.
+ * The image starts at column 4 goes to column 9.
+ * The border starts at column 8
+ */
+export const ContentOverlapLandscape = component$(
+  (props: ContentOverlapLandscapeProps) => {
+    return (
+      <article class="grid auto-rows-min md:grid-cols-8 md:grid-rows-1">
+        <div class="z-20 row-start-3 -translate-y-28 md:translate-y-60 xl:-translate-x-20 mx-3 pt-10 pb-10 pr-5 pl-5 md:row-span-full md:col-start-3 md:col-span-6 md:p-14 shadow-md  self-center bg-white">
+          <Slot name="article"></Slot>
+        </div>
+        <div class="z-10 row-start-1 md:row-span-full md:col-start-3 md:col-span-6 md:-translate-x-8 xl:-translate-x-28">
+          <img src={props.image.source} alt={props.image.alternateText} />
+        </div>
+        <div class="z-0 row-start-2 md:row-span-full bg-primary md:col-start-2 md:col-span-2 md:self-end md:translate-y-10 h-4 md:w-48 md:h-[32rem] xl:h-[40rem]"></div>
+      </article>
+    );
+  }
+);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,55 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { ArticleTriad } from '../components/article-triad/article-triad';
+import { ContentOverlapLandscape } from '../components/content-overlap/content-overlap-landscape';
+import { ContentOverlapPortrait } from '../components/content-overlap/content-overlap-portrait';
 import { Heading } from '../components/heading/heading';
 import { LinkCallToAction } from '../components/link-call-to-action/link-call-to-action';
 
 export default component$(() => {
   return (
-    <div class="container mx-auto">
-      <ArticleTriad>
+    <div class="container mx-auto flex flex-col space-y-96">
+      <ContentOverlapPortrait
+        image={{
+          source: '/img/landing-page/developers-tablet.jpg',
+          alternateText: '2 Developers checking an App on a tablet'
+        }}
+      >
         <section q:slot="article" class="flex flex-col space-y-4">
           <Heading>
             <span q:slot="primary">Denken Sie auch in</span>
             <span q:slot="accent">ganzheitlichen Lösungen?</span>
           </Heading>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
-            repellat eveniet atque incidunt, praesentium quia sequi quidem
-            dolorem amet quos eos ex voluptatibus blanditiis magni reprehenderit
-            corporis maxime inventore laboriosam.
+          <p class="leading-7">
+            Dann bringen wir Ihre Ideen auf die Straße, ebenen Ihnen neue Wege
+            und schaffen gemeinsam nachhaltige Werte. Unsere Leidenschaft ist es
+            Veränderungen zu etwas Positivem zu machen.
           </p>
           <LinkCallToAction href="/">Mehr erfahren</LinkCallToAction>
         </section>
-        <img q:slot="image" src="/img/landing-page/developers-tablet.jpg"></img>
-      </ArticleTriad>
+      </ContentOverlapPortrait>
+
+      <ContentOverlapLandscape
+        image={{
+          source: '/img/landing-page/cooking-session.jpg',
+          alternateText: 'Our team cooking a delicious meal'
+        }}
+      >
+        <section q:slot="article" class="flex flex-col space-y-4">
+          <Heading>
+            <span q:slot="primary">Machen Menschen für Sie auch den</span>
+            <span q:slot="accent">Unterschied?</span>
+          </Heading>
+          <p class="leading-7">
+            Dann haben wir für Sie echte Game Changer in unseren Reihen.
+            Motiviert durch den Fachkräftemangel, rekrutieren wir mit eigenen
+            HR-Spezialisten und qualitätsgetriebenem Auswahlprozess unsere
+            Talente. Dank unserer Werteorientierten Unternehmenskultur und
+            unserer Team-zentrierten Lösungsmentalität halten und entwickeln wir
+            unsere Mitarbeiter.
+          </p>
+          <LinkCallToAction href="/">Mehr erfahren</LinkCallToAction>
+        </section>
+      </ContentOverlapLandscape>
     </div>
   );
 });
