fa8da111909cc6ab63dfe2dbe1e4dd7745b50479
christopher saburo <christopher.sab@outlook.com>
2023-01-23 07:05:25 -0800

    completed footer desktop initial version

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,4 +1,7 @@
 import { component$ } from "@builder.io/qwik";
+import { GithubIcon } from "../icons/GithubIcon";
+import { MapIcon } from "../icons/MapIcon";
+import { YoutubeIcon } from "../icons/YoutubeIcon";
 
 export const Footer = component$(() => {
   return (
@@ -43,14 +46,18 @@ export const Footer = component$(() => {
             <div class="text-lg font-bold leading-5 text-secondary">co-IT.eu GmbH</div>
 
             <div class="flex flex-col gap-y-1">
-              <div class="text-lg font-normal leading-5 text-secondary">Eisenbahnstr.15</div>
-              <div class="text-lg font-normal leading-5 text-secondary">77855 Achern</div>
+              <div class="text-lg font-normal leading-5 text-secondary">Kriegsstr. 39</div>
+              <div class="text-lg font-normal leading-5 text-secondary">76133 Karlsruhe</div>
             </div>
 
-            <div class="bg-secondary text-lg font-semibold leading-5 text-white px-4 py-2 rounded">
-              Zu Google Maps
+            <div class="bg-secondary text-lg font-semibold leading-5 text-white px-4 py-2 rounded flex flex-row gap-x-2">
+              <MapIcon class="my-auto" />
+              <span>
+                Zu Google Maps
+              </span>
             </div>
           </div>
+
           <div class="bg-white px-10 py-9 flex flex-col gap-y-7 shadow-lg">
             <div class="text-lg font-bold leading-5 text-secondary">co-IT.eu GmbH</div>
 
@@ -59,26 +66,30 @@ export const Footer = component$(() => {
               <div class="text-lg font-normal leading-5 text-secondary">77855 Achern</div>
             </div>
 
-            <div class="bg-secondary text-lg font-semibold leading-5 text-white px-4 py-2 rounded">
-              Zu Google Maps
+            <div class="bg-secondary text-lg font-semibold leading-5 text-white px-4 py-2 rounded flex flex-row gap-x-2">
+              <MapIcon class="my-auto" />
+              <span>
+                Zu Google Maps
+              </span>
             </div>
           </div>
+
           <div class="bg-white px-10 py-9 flex flex-col gap-y-7 shadow-lg">
-            <div class="text-lg font-bold leading-5 text-secondary">co-IT.eu GmbH</div>
+            <div class="text-lg font-bold leading-5 text-secondary">Weitere co-IT Kanäle</div>
 
-            <div class="flex flex-col gap-y-1">
-              <div class="text-lg font-normal leading-5 text-secondary">Eisenbahnstr.15</div>
-              <div class="text-lg font-normal leading-5 text-secondary">77855 Achern</div>
-            </div>
-
-            <div class="bg-secondary text-lg font-semibold leading-5 text-white px-4 py-2 rounded">
-              Zu Google Maps
+            <div class="flex flex-row gap-x-7">
+              <YoutubeIcon />
+              <GithubIcon />
             </div>
           </div>
         </div>
       </div>
 
-      <div class="bg-[#ffc138] py-5 mt-48"></div>
+      <div class="bg-[#ffc138] py-5 mt-48 flex justify-center">
+        <span class="text-xs text-black font-normal opacity-40">
+          © 2022 - co-IT GmbH   -  Datenschutz    Impressum   AGB
+        </span>
+      </div>
     </>
   );
 });
