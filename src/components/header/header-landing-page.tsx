711b38471a54dca452ebfb3ac5ec93901dc76786
christopher saburo <christopher.sab@outlook.com>
2023-01-23 09:47:37 -0800

    added menu icon section

--- src/components/header/header-landing-page.tsx
+++ src/components/header/header-landing-page.tsx
@@ -2,6 +2,7 @@ import { component$ } from '@builder.io/qwik';
 import HeadingSegmentAccent from '../heading-segment/heading-segment-accent';
 import HeadingSegmentWhite from '../heading-segment/heading-segment-white';
 import type { HeaderProps } from './header.props';
+import { MenuIcon } from '../icons/menu-icon';
 
 export default component$((props: HeaderProps) => {
   return (
@@ -10,13 +11,20 @@ export default component$((props: HeaderProps) => {
       style={`background-image: linear-gradient(0deg, rgba(2, 2, 52, 0.6), rgba(2, 2, 52, 0.6)), url("${props.backgroundImage.source}");`}
     >
       <div class="container mx-auto grid grid-rows-2 place-content-center text-center space-y-32">
-        <section class="">
+        <section class="flex justify-between h-fit">
           <img
             width="173"
             height="69"
             src="/img/logo.svg"
             alt="co-IT.eu orange Logo - Inspire to Change"
           />
+
+          <div class="flex flex-row gap-x-4 my-auto cursor-pointer hover:opacity-75">
+            <MenuIcon />
+            <span class="text-2xl text-white font-bold leading-7 my-auto">
+              Menu
+            </span>
+          </div>
         </section>
         <section class="flex flex-col space-y-10 pb-16">
           <h1 class="break-words font-bold text-2xl md:text-6xl">
