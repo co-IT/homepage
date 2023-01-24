e3ca4af8711aca60db47308125de895620f1301a
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-01-24 21:56:27 +0100

    feat: optimize mobile view

--- src/components/header/header-landing-page/header-landing-page.tsx
+++ src/components/header/header-landing-page/header-landing-page.tsx
@@ -10,22 +10,22 @@ import type { HeaderLandingPageProps } from './header-landing-page.props';
 export const HeaderLandingPage = component$((props: HeaderLandingPageProps) => {
   return (
     <header
-      class="bg-cover bg-no-repeat bg-right-top py-16"
+      class="bg-cover bg-no-repeat md:bg-right-top"
       style={`background-image: linear-gradient(0deg, rgba(2, 2, 52, 0.6), rgba(2, 2, 52, 0.6)), url("${props.backgroundImage.source}");`}
     >
-      <div class="container mx-auto grid grid-rows-2 place-content-center text-center space-y-32">
-        <section class="flex justify-between h-fit">
+      <div class="container mx-auto grid grid-rows-2 md:place-content-center text-center ">
+        <section class="flex justify-center md:justify-between pt-6 h-fit">
           <Logo></Logo>
 
-          <div class="flex flex-row gap-x-4 my-auto cursor-pointer hover:opacity-75">
+          <div class="hidden md:block flex-row gap-x-4 my-auto cursor-pointer hover:opacity-75">
             <MenuIcon />
             <span class="text-2xl text-white font-bold leading-7 my-auto">
               Menu
             </span>
           </div>
         </section>
-        <section class="flex flex-col space-y-10">
-          <h1 class="break-words font-bold text-2xl md:text-6xl">
+        <section class="flex flex-col items-center space-y-10">
+          <h1 class="font-bold text-2xl md:text-6xl">
             {props.headingSegments.map(headingSegment => (
               <>
                 {headingSegment.type === 'white' ? (
@@ -39,7 +39,7 @@ export const HeaderLandingPage = component$((props: HeaderLandingPageProps) => {
           </h1>
 
           <div class=" bg-secondary md:bg-opacity-0">
-            <p class="text-lg leading-10 mx-auto max-w-3xl text-white font-semibold  tracking-wider md:text-xl md:leading-10 md:[text-shadow:0_4px_8px_rgba(0,0,0,0.8)]">
+            <p class="text-lg leading-8 tracking-wider max-w-3xl text-white p-8 md:font-semibold md:text-xl md:leading-10 md:[text-shadow:0_4px_8px_rgba(0,0,0,0.8)]">
               {props.teaser}
             </p>
           </div>
