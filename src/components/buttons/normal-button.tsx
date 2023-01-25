50fb26ca325c3cdec6a3cd99b00d986169d900ca
christopher saburo <christopher.sab@outlook.com>
2023-01-25 07:37:33 -0800

    fixed button commits

--- src/components/buttons/normal-button.tsx
+++ src/components/buttons/normal-button.tsx
@@ -6,10 +6,8 @@ interface ButtonProps {
 
 export const NormalButton = component$((props: ButtonProps) => {
   return (
-    <>
-      <div class="px-7 py-2.5 hover:opacity-75 bg-primary w-fit rounded cursor-pointer text-lg font-semibold leading-5 text-secondary">
-        {props.title}
-      </div>
-    </>
+    <button class="px-7 py-2.5 hover:opacity-75 bg-primary w-fit rounded cursor-pointer text-lg font-semibold leading-5 text-secondary">
+      {props.title}
+    </button>
   )
 });
