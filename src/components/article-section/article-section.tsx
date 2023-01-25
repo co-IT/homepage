d65d8af153a89e3cb41d93b2059aaceefbece958
christopher saburo <christopher.sab@outlook.com>
2023-01-25 07:36:20 -0800

    fixed article commits

--- src/components/article-section/article-section.tsx
+++ src/components/article-section/article-section.tsx
@@ -2,24 +2,43 @@ import { component$ } from "@builder.io/qwik";
 import { ArticleCard } from "../cards";
 
 export const ArticleSection = component$(() => {
+  const articles = [
+    {
+      direction: "right",
+      heading: "Wir meistern Komplexität im Team",
+      text: "Unsere T-Shaped Professionals flankieren ihr Tiefenwissen mit ausgewähltem Breitenwissen. Diesen Gedanken haben wir auf Unternehmensebene konsquent weitergedacht, indem wir gezielt Kompetenzen einzelner Mitarbeiter aufeinander abstimmen. Beispielsweise bleibt ein Haus, bei dem lediglich die Eingangstür, nicht aber die Fenster verschlossen sind, unsicher. Darum haben wir sowohl im IT-Serivce-Management als auch in der Softwareentwicklung zertifizierte Sicherheitsexperten.",
+      imageSource: "/img/quality/header.avif",
+      imageAlternateText: "article"
+    },
+    {
+      direction: "left",
+      heading: "Wir meistern Komplexität im Team",
+      text: "Unsere T-Shaped Professionals flankieren ihr Tiefenwissen mit ausgewähltem Breitenwissen. Diesen Gedanken haben wir auf Unternehmensebene konsquent weitergedacht, indem wir gezielt Kompetenzen einzelner Mitarbeiter aufeinander abstimmen. Beispielsweise bleibt ein Haus, bei dem lediglich die Eingangstür, nicht aber die Fenster verschlossen sind, unsicher. Darum haben wir sowohl im IT-Serivce-Management als auch in der Softwareentwicklung zertifizierte Sicherheitsexperten.",
+      imageSource: "/img/quality/header.avif",
+      imageAlternateText: "article"
+    },
+    {
+      direction: "right",
+      heading: "Wir meistern Komplexität im Team",
+      text: "Unsere T-Shaped Professionals flankieren ihr Tiefenwissen mit ausgewähltem Breitenwissen. Diesen Gedanken haben wir auf Unternehmensebene konsquent weitergedacht, indem wir gezielt Kompetenzen einzelner Mitarbeiter aufeinander abstimmen. Beispielsweise bleibt ein Haus, bei dem lediglich die Eingangstür, nicht aber die Fenster verschlossen sind, unsicher. Darum haben wir sowohl im IT-Serivce-Management als auch in der Softwareentwicklung zertifizierte Sicherheitsexperten.",
+      imageSource: "/img/quality/header.avif",
+      imageAlternateText: "article"
+    }
+  ];
   return (
-    <>
-      <div class="w-full container mx-auto flex flex-col gap-y-20">
+    <div class="w-full container mx-auto flex flex-col gap-y-20">
+      {articles.map(article => (
         <div>
-          <ArticleCard direction="right" />
+          <ArticleCard
+            direction={article.direction}
+            heading={article.heading}
+            text={article.text}
+            imageSource={article.imageSource}
+            imageAlternateText={article.imageAlternateText}
+          />
           <hr class="mt-20" />
         </div>
-
-        <div>
-          <ArticleCard direction="left" />
-          <hr class="mt-20" />
-        </div>
-
-        <div>
-          <ArticleCard direction="right" />
-          <hr class="mt-20" />
-        </div>
-      </div>
-    </>
+      ))}
+    </div>
   )
 });
--- src/components/cards/article-card.tsx
+++ src/components/cards/article-card.tsx
@@ -1,10 +1,15 @@
 import { component$ } from "@builder.io/qwik";
 
-interface CardProps {
-  direction: string
+interface ArticleCardProps {
+  direction: "left" | "right";
+  imageSource: string;
+  imageAlternateText: string;
+  headingSmall: string;
+  heading: string;
+  text: string;
 }
 
-export const ArticleCard = component$((props: CardProps) => {
+export const ArticleCard = component$((props: ArticleCardProps) => {
   return (
     <>
       {props.direction == "right" ?
@@ -13,36 +18,35 @@ export const ArticleCard = component$((props: CardProps) => {
             <div class="flex flex-col gap-y-4">
               <div class="w-10 h-1 bg-accent" />
               <div class="text-[40px] text-secondary font-bold leading-[50px]">
-                Wir meistern Komplexität im Team
+                {props.heading}
               </div>
             </div>
 
             <div class="text-base text-black font-normal leading-7 opacity-80 tracking-normal">
-              Unsere T-Shaped Professionals flankieren ihr Tiefenwissen mit ausgewähltem Breitenwissen. Diesen Gedanken haben wir auf Unternehmensebene konsquent weitergedacht, indem wir gezielt Kompetenzen einzelner Mitarbeiter aufeinander abstimmen.
-              Beispielsweise bleibt ein Haus, bei dem lediglich die Eingangstür, nicht aber die Fenster verschlossen sind, unsicher. Darum haben wir sowohl im IT-Serivce-Management als auch in der Softwareentwicklung zertifizierte Sicherheitsexperten.
+              {props.text}
             </div>
           </div>
 
           <div class="w-full">
-            <img src="/img/quality/header.avif" alt="article" />
+            <img src={props.imageSource} alt={props.imageAlternateText} />
           </div>
         </div>
         :
         <div class="px-10 flex flex-row gap-x-12">
           <div class="w-full">
-            <img src="/img/quality/header.avif" alt="article" />
+            <img src={props.imageSource} alt={props.imageAlternateText} />
           </div>
 
           <div class="w-full flex flex-col gap-y-5">
             <div class="flex flex-col gap-y-4">
               <div class="w-10 h-1 bg-accent" />
               <div class="text-[40px] text-secondary font-bold leading-[50px]">
-                Wir legen den Finger in die Wunde
+                {props.heading}
               </div>
             </div>
 
             <div class="text-base text-black font-normal leading-7 opacity-80 tracking-normal">
-              Um das Business zu verstehen, müssen wir Menschen verstehen! Wenn beispielsweise eine Software für das Wissensmanagement - wie ein Unternehmenswiki - eingeführt werden soll, stellen wir die Frage nach der Bereitschaft der Angestellten ihr wertvolles Wissen zu teilen. Hierfür sind sichere Arbeitsplätze und eine echte Vertrauenskultur Voraussetzung.
+              {props.text}
             </div>
           </div>
         </div>
