90140f3c18acdc95fa1698bd2dd63bf1720a089a
christopher saburo <christopher.sab@outlook.com>
2023-01-26 23:25:00 +0900

    fixed info card responsive design

--- src/components/cards/article-card.tsx
+++ src/components/cards/article-card.tsx
@@ -17,15 +17,15 @@ export const ArticleCard = component$((props: ArticleCardProps) => {
           <div class="w-full flex flex-col gap-y-5">
             <div class="flex flex-col gap-y-4">
               <div class="w-10 h-1 bg-accent mx-auto lg:mx-0" />
-              <div class="text-[40px] text-secondary font-bold leading-[50px] text-center lg:text-left">
+              <div class="text-[28px] lg:text-[40px] text-secondary font-bold leading-8 lg:leading-[50px] text-center lg:text-left px-6 lg:px-0">
                 {props.heading}
               </div>
-              <div class="w-full lg:hidden">
+              <div class="w-full mt-4 lg:hidden">
                 <img src={props.imageSource} alt={props.imageAlternateText} />
               </div>
             </div>
 
-            <div class="text-base text-black font-normal leading-7 opacity-80 tracking-normal text-center lg:text-left px-10 lg:px-0">
+            <div class="text-base text-black font-normal leading-7 opacity-80 tracking-normal text-left px-10 lg:px-0">
               {props.text}
             </div>
           </div>
@@ -43,15 +43,15 @@ export const ArticleCard = component$((props: ArticleCardProps) => {
           <div class="w-full flex flex-col gap-y-5">
             <div class="flex flex-col gap-y-4">
               <div class="w-10 h-1 bg-accent mx-auto lg:mx-0" />
-              <div class="text-[40px] text-secondary font-bold leading-[50px] text-center lg:text-left">
+              <div class="text-[28px] lg:text-[40px] text-secondary font-bold leading-8 lg:leading-[50px] text-center lg:text-left px-6 lg:px-0">
                 {props.heading}
               </div>
-              <div class="w-full lg:hidden">
+              <div class="w-full mt-4 lg:hidden">
                 <img src={props.imageSource} alt={props.imageAlternateText} />
               </div>
             </div>
 
-            <div class="text-base text-black font-normal leading-7 opacity-80 tracking-normal text-center lg:text-left px-10 lg:px-0">
+            <div class="text-base text-black font-normal leading-7 opacity-80 tracking-normal text-left px-10 lg:px-0">
               {props.text}
             </div>
           </div>
