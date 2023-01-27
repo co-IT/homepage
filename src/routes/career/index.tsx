87f3d41b2335f3876aa6dcd31e224e53c9a16457
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-01-27 13:16:41 +0100

    refactor: simplify code in promise chain

--- src/routes/career/index.tsx
+++ src/routes/career/index.tsx
@@ -9,18 +9,13 @@ import type { JobOfferCompact } from './model';
 import { createJobOffersFromRecruitee } from './model-helper';
 
 export const onGet: RequestHandler<JobOfferCompact[]> = async () => {
-  const response = await fetch('https://coiteugmbh.recruitee.com/api/offers');
-
-  if (!response.ok) {
-    throw new Error('Bad response fetching job offers from Recruitee.');
-  }
-
-  const recruiteeResponse = await response.json();
-
-  const h = createJobOffersFromRecruitee(recruiteeResponse);
-  console.log(h);
-
-  return h;
+  return fetch('https://coiteugmbh.recruitee.com/api/offers')
+    .then(response =>
+      response.ok
+        ? response.json()
+        : Promise.reject('Bad response fetching job offers from Recruitee.')
+    )
+    .then(json => createJobOffersFromRecruitee(json));
 };
 
 export default component$(() => {
