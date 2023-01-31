efac472fe5cae9c7ae9f381faae6c07128bc108d
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-01-31 22:42:23 +0100

    refactor(navigation): move logic from card to navigation

--- src/components/cards/navigation-card.tsx
+++ src/components/cards/navigation-card.tsx
@@ -1,38 +1,40 @@
-import { $, component$, useContext } from "@builder.io/qwik";
-import { useLocation, useNavigate } from "@builder.io/qwik-city";
-import { MenuContext } from "../common";
-import type { MenuStateType } from "../common";
+import type { PropFunction } from '@builder.io/qwik';
+import { component$ } from '@builder.io/qwik';
+import { useLocation } from '@builder.io/qwik-city';
+
+export interface NavigationLink {
+  heading: string;
+  text: string;
+  path: string;
+}
 
 interface NavigationCardProps {
-  title: string;
-  link: string;
-  text: string;
+  link: NavigationLink;
+  onLinkClick$?: PropFunction<(link: NavigationLink) => void>;
 }
 
 export const NavigationCard = component$((props: NavigationCardProps) => {
   const location = useLocation();
   const path = location.pathname;
-  const menuState = useContext<MenuStateType>(MenuContext);
-  const navigation = useNavigate();
-  const clickNavigation = $((link: string) => {
-    navigation.path = link;
-    menuState.isOpen = false;
-  });
 
   return (
     <div
-      class={ path === props.link + '/' ? "w-full bg-white rounded-xl px-4 py-10 flex flex-col gap-y-6 items-center cursor-pointer hover:opacity-100" : "w-full bg-[#ffcd61] rounded-xl px-4 py-10 flex flex-col gap-y-6 items-center cursor-pointer hover:bg-white" }
-      onClick$={() => clickNavigation(props.link)}
+      class={
+        path === props.link.path + '/'
+          ? 'w-full bg-white rounded-xl px-4 py-10 flex flex-col gap-y-6 items-center cursor-pointer hover:opacity-100'
+          : 'w-full bg-[#ffcd61] rounded-xl px-4 py-10 flex flex-col gap-y-6 items-center cursor-pointer hover:bg-white'
+      }
+      onClick$={() => props.onLinkClick$?.(props.link)}
     >
-      <div class="text-lg text-secondary font-bold leading-5">
-        {props.title}
-      </div>
+      <h2 class="text-lg text-secondary font-bold leading-5">
+        {props.link.heading}
+      </h2>
 
       <div class="h-0.5 w-4 bg-black" />
 
-      <div class="text-lg text-secondary font-semibold leading-5 text-center">
-        {props.text}
-      </div>
+      <p class="text-lg text-secondary font-semibold leading-5 text-center">
+        {props.link.text}
+      </p>
     </div>
-  )
+  );
 });
--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,45 +1,66 @@
-import { $, component$, useContext } from "@builder.io/qwik";
-import { NavigationCard } from "../cards";
-import { MenuContext } from "../common";
-import type { MenuStateType } from "../common";
-import { BlueMailIcon, BlueMapIcon, BluePhoneIcon, YellowCloseIcon } from "../icons";
-import { WhiteCloseIcon } from "../icons/white-close-icon";
+import { $, component$, useContext } from '@builder.io/qwik';
+import { useNavigate } from '@builder.io/qwik-city';
+import type { NavigationLink } from '../cards';
+import { NavigationCard } from '../cards';
+import type { MenuStateType } from '../common';
+import { MenuContext } from '../common';
+import {
+  BlueMailIcon,
+  BlueMapIcon,
+  BluePhoneIcon,
+  YellowCloseIcon
+} from '../icons';
+import { WhiteCloseIcon } from '../icons/white-close-icon';
 
 export const Navigation = component$(() => {
-  const NavigationCardItems = [
+  const navigationLinks: NavigationLink[] = [
     {
-      title: "Lösungen",
-      link: "/solutions",
-      text: "Alles aus einer Hand"
+      heading: 'Lösungen',
+      path: '/solutions',
+      text: 'Alles aus einer Hand'
     },
     {
-      title: "Qualität",
-      link: "/quality",
-      text: "Auf den Punkt abgestimmt"
+      heading: 'Qualität',
+      path: '/quality',
+      text: 'Auf den Punkt abgestimmt'
     },
     {
-      title: "Karriere",
-      link: "/career",
-      text: "Geprägt durch Menschen"
+      heading: 'Karriere',
+      path: '/career',
+      text: 'Geprägt durch Menschen'
     },
     {
-      title: "Über uns",
-      link: "/about",
-      text: "Motiviert von Werten"
+      heading: 'Über uns',
+      path: '/about',
+      text: 'Motiviert von Werten'
     }
   ];
 
   const menuState = useContext<MenuStateType>(MenuContext);
+  const navigate = useNavigate();
+
   const closeMenu = $(() => {
     menuState.isOpen = false;
+  });
+
+  const goto$ = $((link: NavigationLink) => {
+    menuState.isOpen = false;
+    navigate(link.path);
   });
 
   return (
     <div class="w-full bg-primary pt-12 lg:pt-16 pb-12 lg:pb-24 absolute top-0 z-10 shadow-xl">
       <div class="container mx-auto flex flex-col gap-y-14 lg:gap-y-20">
         <div class="flex justify-between flex-col lg:flex-row">
-          <img src="/img/navigation/logo.svg" alt="logo" class="w-32 lg:w-44 h-fit mx-auto lg:mx-0" />
-          <div class="flex-row gap-x-2 my-auto cursor-pointer hover:opacity-75 hidden lg:flex" onClick$={() => closeMenu()}>
+          <img
+            src="/img/navigation/logo.svg"
+            alt="logo"
+            class="w-32 lg:w-44 h-fit mx-auto lg:mx-0"
+          />
+          <div
+            class="flex-row gap-x-2 my-auto cursor-pointer hover:opacity-75 hidden lg:flex"
+            onClick$={() => closeMenu()}
+          >
             <WhiteCloseIcon />
             <div class="text-lg text-white font-bold leading-5 my-auto">
               Schließen
@@ -49,13 +70,20 @@ export const Navigation = component$(() => {
 
         <div class="flex flex-col gap-y-14">
           <div class="grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-10 px-7 lg:px-0">
-            {NavigationCardItems.map((cardItem, key) => (
-              <NavigationCard title={cardItem.title} link={cardItem.link} text={cardItem.text} key={key} />
+            {navigationLinks.map((navigationLink, key) => (
+              <NavigationCard
+                link={navigationLink}
+                onLinkClick$={link => goto$(link)}
+                key={key}
+              />
             ))}
           </div>
 
           <div class="flex justify-center">
-            <a href="/contact" class="bg-secondary hover:opacity-75 text-white font-bold text-lg w-fit px-14 py-5 rounded">
+            <a
+              href="/contact"
+              class="bg-secondary hover:opacity-75 text-white font-bold text-lg w-fit px-14 py-5 rounded"
+            >
               Kontakt aufnehmen
             </a>
           </div>
@@ -92,7 +120,10 @@ export const Navigation = component$(() => {
             </a>
           </div>
 
-          <div class="w-14 h-14 bg-white rounded-full shadow-lg relative" onClick$={() => closeMenu()}>
+          <div
+            class="w-14 h-14 bg-white rounded-full shadow-lg relative"
+            onClick$={() => closeMenu()}
+          >
             <div class="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
               <YellowCloseIcon />
             </div>
@@ -100,5 +131,5 @@ export const Navigation = component$(() => {
         </div>
       </div>
     </div>
-  )
+  );
 });
