d804fddf455c1a03d902846ca71b7d38c3a4e8a8
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-02-01 22:38:15 +0100

    fix(navigation): make navigation fix.

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -39,9 +39,7 @@ export const Navigation = component$(() => {
   const navigation = useContext<NavigationState>(NavigationContext);
   const navigate = useNavigate();
 
-  const closeMenu = $(() => {
-    navigation.isOpen = false;
-  });
+  const closeMenu = $(() => (navigation.isOpen = false));
 
   const goto$ = $((link: NavigationLink) => {
     navigation.isOpen = false;
@@ -49,27 +47,27 @@ export const Navigation = component$(() => {
   });
 
   return (
-    <div class="w-full bg-primary pt-12 lg:pt-16 pb-12 lg:pb-24 absolute top-0 z-20 shadow-xl">
-      <div class="container mx-auto flex flex-col gap-y-14 lg:gap-y-20">
-        <div class="flex justify-between flex-col lg:flex-row">
+    <div class='w-full bg-primary pt-12 lg:pt-16 pb-12 lg:pb-24 fixed top-0 z-30 shadow-xl'>
+      <div class='container mx-auto flex flex-col gap-y-14 lg:gap-y-20'>
+        <div class='flex justify-between flex-col lg:flex-row'>
           <img
-            src="/img/navigation/logo.svg"
-            alt="logo"
-            class="w-32 lg:w-44 h-fit mx-auto lg:mx-0"
+            src='/img/navigation/logo.svg'
+            alt='logo'
+            class='w-32 lg:w-44 h-fit mx-auto lg:mx-0'
           />
           <div
-            class="flex-row gap-x-2 my-auto cursor-pointer hover:opacity-75 hidden lg:flex"
+            class='flex-row gap-x-2 my-auto cursor-pointer hover:opacity-75 hidden lg:flex'
             onClick$={() => closeMenu()}
           >
             <WhiteCloseIcon />
-            <div class="text-lg text-white font-bold leading-5 my-auto">
+            <div class='text-lg text-white font-bold leading-5 my-auto'>
               Schließen
             </div>
           </div>
         </div>
 
-        <div class="flex flex-col gap-y-14">
-          <div class="grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-10 px-7 lg:px-0">
+        <div class='flex flex-col gap-y-14'>
+          <div class='grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-10 px-7 lg:px-0'>
             {navigationLinks.map((navigationLink, key) => (
               <NavigationLinkCard
                 link={navigationLink}
@@ -79,52 +77,52 @@ export const Navigation = component$(() => {
             ))}
           </div>
 
-          <div class="flex justify-center">
+          <div class='flex justify-center'>
             <a
-              href="/contact"
-              class="bg-secondary hover:opacity-75 text-white font-bold text-lg w-fit px-14 py-5 rounded"
+              href='/contact'
+              class='bg-secondary hover:opacity-75 text-white font-bold text-lg w-fit px-14 py-5 rounded'
             >
               Kontakt aufnehmen
             </a>
           </div>
         </div>
 
-        <div class="flex lg:hidden flex-row justify-between px-10">
-          <div class="flex flex-row gap-x-5">
+        <div class='flex lg:hidden flex-row justify-between px-10'>
+          <div class='flex flex-row gap-x-5'>
             <a
-              href="tel:+49721935163052"
-              target="_self"
-              class="w-14 h-14 bg-white rounded-full shadow-lg relative"
+              href='tel:+49721935163052'
+              target='_self'
+              class='w-14 h-14 bg-white rounded-full shadow-lg relative'
             >
-              <div class="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
+              <div class='absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4'>
                 <BluePhoneIcon />
               </div>
             </a>
             <a
-              href="mailto:info@co-IT.eu"
-              target="_blank"
-              class="w-14 h-14 bg-white rounded-full shadow-lg relative"
+              href='mailto:info@co-IT.eu'
+              target='_blank'
+              class='w-14 h-14 bg-white rounded-full shadow-lg relative'
             >
-              <div class="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
+              <div class='absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4'>
                 <BlueMailIcon />
               </div>
             </a>
             <a
-              href="https://g.page/communicativeIT?share"
-              target="_blank"
-              class="w-14 h-14 bg-white rounded-full shadow-lg relative"
+              href='https://g.page/communicativeIT?share'
+              target='_blank'
+              class='w-14 h-14 bg-white rounded-full shadow-lg relative'
             >
-              <div class="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
+              <div class='absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4'>
                 <BlueMapIcon />
               </div>
             </a>
           </div>
 
           <div
-            class="w-14 h-14 bg-white rounded-full shadow-lg relative"
+            class='w-14 h-14 bg-white rounded-full shadow-lg relative'
             onClick$={() => closeMenu()}
           >
-            <div class="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
+            <div class='absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4'>
               <YellowCloseIcon />
             </div>
           </div>
