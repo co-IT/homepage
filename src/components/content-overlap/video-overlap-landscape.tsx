0669d10bef48846a5cbafe4a5a8366cfc56be40f
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-02-01 22:30:50 +0100

    fix(video): adjust overlap on lorger screens

--- src/components/content-overlap/video-overlap-landscape.tsx
+++ src/components/content-overlap/video-overlap-landscape.tsx
@@ -1,12 +1,5 @@
 import { component$, Slot } from '@builder.io/qwik';
 
-interface ContentOverlapLandscapeProps {
-  image: {
-    source: string;
-    alternateText: string;
-  };
-}
-
 export const VideoOverlapLandscape = component$(
   (props: { youTubeVideoId: string }) => {
     return (
@@ -15,7 +8,7 @@ export const VideoOverlapLandscape = component$(
           <Slot></Slot>
         </div>
         <iframe
-          class='z-20 aspect-video w-full row-start-1 md:row-start-2 md:col-start-3 md:col-span-6 md:-translate-y-40 md:-translate-x-8 xl:-translate-x-28'
+          class='z-20 aspect-video w-full row-start-1 md:row-start-2 md:col-start-3 md:col-span-6 md:-translate-y-32 lg:-translate-y-52 xl:-translate-y-72 md:-translate-x-8 xl:-translate-x-28'
           src={`https://www.youtube.com/embed/${props.youTubeVideoId}`}
         ></iframe>
         <div class='z-0 row-start-2 md:row-start-1 md:row-span-full bg-primary md:col-start-2 md:col-span-2 md:self-end md:translate-y-10 h-36 md:w-48 md:h-[32rem] xl:h-[40rem]'></div>
