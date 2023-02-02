452bc00213cc1a014e8c90e709c5980095b92566
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-02-02 23:49:31 +0100

    feat(about): add texts

--- src/components/article-section/article-section.tsx
+++ src/components/article-section/article-section.tsx
@@ -5,22 +5,22 @@ export const ArticleSection = component$(() => {
   const articles = [
     {
       direction: 'right',
-      heading: 'Wir meistern Komplexität im Team',
-      text: 'Unsere T-Shaped Professionals flankieren ihr Tiefenwissen mit ausgewähltem Breitenwissen. Diesen Gedanken haben wir auf Unternehmensebene konsquent weitergedacht, indem wir gezielt Kompetenzen einzelner Mitarbeiter aufeinander abstimmen. Beispielsweise bleibt ein Haus, bei dem lediglich die Eingangstür, nicht aber die Fenster verschlossen sind, unsicher. Darum haben wir sowohl im IT-Serivce-Management als auch in der Softwareentwicklung zertifizierte Sicherheitsexperten.',
+      heading: 'Für Veränderungen begeistern',
+      text: 'Das Meistern neuer Herausforderungen sehen wir als größte Hürde für kleine und mittelständische Unternehmen. Deshalb wollen wir helfen Veränderung als Chance wahrzunehmen und Erneuerungen zu etwas Positivem machen. Uns prägen Werte wie Pflichtgefühl und Kollegialität. Daher gehört der ehrliche und verbindliche Handschlag für unsere Mitarbeiter auch in der digitalen Welt dazu.',
       imageSource: '/img/about/flip-chart-session.avif',
       imageAlternateText: 'article'
     },
     {
       direction: 'left',
-      heading: 'Wir meistern Komplexität im Team',
-      text: 'Unsere T-Shaped Professionals flankieren ihr Tiefenwissen mit ausgewähltem Breitenwissen. Diesen Gedanken haben wir auf Unternehmensebene konsquent weitergedacht, indem wir gezielt Kompetenzen einzelner Mitarbeiter aufeinander abstimmen. Beispielsweise bleibt ein Haus, bei dem lediglich die Eingangstür, nicht aber die Fenster verschlossen sind, unsicher. Darum haben wir sowohl im IT-Serivce-Management als auch in der Softwareentwicklung zertifizierte Sicherheitsexperten.',
+      heading: 'Zusammen mehr erreichen',
+      text: 'Wir haben den Anspruch Ihnen Spitzenleistungen zu bieten. Uns ist bewusst, dass es dafür eine partnerschaftliche Zusammenarbeit auf Augenhöhe braucht. Die Technologie kommt dabei zum Schluss. Zuerst analysieren wir daher gemeinsam Ihre fachlichen Bedürfnisse und unternehmensspezifischen Hintergrund. Erst im Anschluss bringen wir unser technisches Tiefen- und prozessuales Breitenwissen ein, um Ihre Geschäftsprozesse bestmöglich umzusetzen. Unsere Philosophie ist darauf ausgerichtet Qualitätskriterien für modernes, effektives und angenehmes Arbeiten umzusetzen, damit wir in einem kontinuierlichen Verbesserungsprozess die Weichen sukzessive auf nachhaltige, erfolgreiche Veränderung stellen. Regelmäßig bedarf es dafür nicht nur das Wissen darüber, was funktioniert, sondern eben auch was nicht funktioniert.',
       imageSource: '/img/about/team-up.avif',
       imageAlternateText: 'article'
     },
     {
       direction: 'right',
-      heading: 'Wir meistern Komplexität im Team',
-      text: 'Unsere T-Shaped Professionals flankieren ihr Tiefenwissen mit ausgewähltem Breitenwissen. Diesen Gedanken haben wir auf Unternehmensebene konsquent weitergedacht, indem wir gezielt Kompetenzen einzelner Mitarbeiter aufeinander abstimmen. Beispielsweise bleibt ein Haus, bei dem lediglich die Eingangstür, nicht aber die Fenster verschlossen sind, unsicher. Darum haben wir sowohl im IT-Serivce-Management als auch in der Softwareentwicklung zertifizierte Sicherheitsexperten.',
+      heading: 'Uns prägt Faszination',
+      text: 'Speziellen Wert legen wir deshalb auf die Auswahl unserer Mitarbeiter. Wir wissen, dass Faszination und Leidenschaft zu Höchstleistungen motivieren und wo diese sind, ist Kollegialität auch nicht weit. Die gute Zusammenarbeit hat besondere Vorteile für Sie: Lösungen können entlang der gesamten Wertschöpfungskette entwickelt werden. Der Übergang von der Prozessanalyse über das Bereitstellen der zuverlässigen Infrastruktur bis hin zur individuellen Softwareentwicklung geschieht nahtlos und liefert Ihnen einen echten Mehrwert.',
       imageSource: '/img/about/it-fascination.avif',
       imageAlternateText: 'article'
     }
