0b44e7201101aca86745d4527dc5719382718aac
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-02-06 21:05:46 +0100

    chore: update qwik (#42)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import {
-  ContentOverlapLandscape,
   ContentOverlapPortrait,
   VideoOverlapLandscape
 } from '~/components/content-overlap';
