82d18d61bcbd074cdf84a4be8290ff3a795a1c8f
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-02-07 20:39:11 +0100

    fix: improve recruiting cards

---
+++ src/components/job-offers/job-offer-card.tsx
@@ -0,0 +1,37 @@
+import { component$ } from '@builder.io/qwik';
+import type { JobOfferCompact } from '../../routes/career/model';
+import { LinkCallToAction } from '../link-call-to-action';
+
+export interface JobOfferCardProps {
+  jobOffer: JobOfferCompact;
+}
+
+export const JobOfferCard = component$((props: JobOfferCardProps) => {
+  return (
+    <div class='grid grid-cols-2 gap-y-4 rounded-xl border border-gray-300 p-6'>
+      <h2 class='text-secondary break-all text-2xl font-bold'>
+        {props.jobOffer.title}
+      </h2>
+
+      <LinkCallToAction
+        href={props.jobOffer.offerUrl}
+        target='_blank'
+        class='inline-flex h-fit justify-end'
+      >
+        Job ansehen
+      </LinkCallToAction>
+
+      <div class='col-span-2'>
+        <span>{props.jobOffer.location}</span>
+
+        <div class='flex flex-row gap-2'>
+          {props.jobOffer.tags?.map(tag => (
+            <span class='bg-secondary rounded-lg px-2 py-1 text-sm font-bold leading-5 text-white'>
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+});
