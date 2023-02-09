e4654cde46bdbecf522eb9f3d47f549c6d72c549
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-02-09 11:18:56 +0100

    feat(video): improve youtube consent overlay

--- src/components/youtube/youtube-video-player-embedded.tsx
+++ src/components/youtube/youtube-video-player-embedded.tsx
@@ -24,38 +24,35 @@ export const YoutubeVideoPlayerEmbedded = component$(
     });
 
     return (
-      <>
-        {!hasVisitorAgreedToYouTubeTermsOfUse.value ? (
-          <div
-            class={`mx-auto mb-5 grid aspect-video place-items-center content-center gap-y-6 border-0 bg-black/80 text-white backdrop-blur-md ${props.class}`}
-          >
-            <p>
-              Durch das Abspielen von Videos stimmen Sie den{' '}
-              <a
-                class='text-primary hover:underline'
-                href='https://www.youtube.com/static?gl=DE&template=terms&hl=de'
-                target='_blank'
-              >
-                Nutzungsbedingungen
-              </a>{' '}
-              von YouTube zu.
-            </p>
-            <button
-              onClick$={() => agreeToYouTubeTermsOfUse$()}
-              class='cursor-pointer rounded-lg bg-primary p-3 font-bold text-secondary-900 hover:bg-primary/90'
+      <div class='relative'>
+        <iframe
+          loading='lazy'
+          src={`https://www.youtube-nocookie.com/embed/${props.youTubeVideoId}`}
+          class={`mx-auto mb-5 aspect-video border-0 ${props.class}`}
+        />
+
+        <div
+          class={`absolute left-0 top-0 mx-auto mb-5 grid aspect-video h-full w-full place-items-center content-center gap-y-6 border-0 bg-black/30 text-white backdrop-blur-md`}
+        >
+          <p class='mb-4 px-6 text-center font-bold'>
+            Durch das Abspielen von Videos stimmen Sie den{' '}
+            <a
+              class='text-primary hover:underline'
+              href='https://www.youtube.com/static?gl=DE&template=terms&hl=de'
+              target='_blank'
             >
-              Ich stimme zu und möchte das Video sehen.
-            </button>
-          </div>
-        ) : (
-          <>
-            <iframe
-              src={`https://www.youtube-nocookie.com/embed/${props.youTubeVideoId}`}
-              class={`mx-auto mb-5 aspect-video border-0 ${props.class}`}
-            />
-          </>
-        )}
-      </>
+              Nutzungsbedingungen
+            </a>{' '}
+            von YouTube zu.
+          </p>
+          <button
+            onClick$={() => agreeToYouTubeTermsOfUse$()}
+            class='cursor-pointer rounded-lg bg-primary p-3 font-bold text-secondary-900 hover:bg-primary/90'
+          >
+            Ich stimme zu und möchte das Video sehen.
+          </button>
+        </div>
+      </div>
     );
   }
 );
