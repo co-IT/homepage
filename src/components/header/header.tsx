1f1d15af3d5fdd5b1a0d18ac8953de5c6bdc7ee4
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-03-24 16:28:33 +0100

    fix(header): decrease height for default-header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -13,6 +13,8 @@ export const Header = component$((props: HeaderProps) => {
   const menuState = useContext<NavigationState>(NavigationContext);
   const openMenu = $(() => (menuState.isOpen = true));
 
+  const headerHeight = props.type === 'default' ? 'lg:h-auto' : 'lg:h-[90vh]';
+
   const headerBackgrundImage =
     props.type !== 'default'
       ? `background-image: linear-gradient(0deg, rgba(2, 2, 52, 0.6), rgba(2, 2, 52, 0.6)), url("${props.configuration.backgroundImage.source}");`
@@ -20,7 +22,7 @@ export const Header = component$((props: HeaderProps) => {
 
   return (
     <header
-      class='bg-secondary-900 bg-cover bg-no-repeat sm:max-h-max md:bg-right-top lg:h-[90vh]'
+      class={`bg-secondary-900 bg-cover bg-no-repeat sm:max-h-max md:bg-right-top ${headerHeight}`}
       style={headerBackgrundImage}
     >
       <div class='container mx-auto grid gap-36 lg:h-full'>
