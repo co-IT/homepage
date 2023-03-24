ef3d3a0f6a1abb4258a9de3d99fad1490559b7a0
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-03-24 17:02:31 +0100

    feat(about): update images

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -21,7 +21,7 @@ export default component$(() => {
       heading: 'Zusammen mehr erreichen',
       text: 'Wir haben den Anspruch Ihnen Spitzenleistungen zu bieten. Uns ist bewusst, dass es dafür eine partnerschaftliche Zusammenarbeit auf Augenhöhe braucht. Die Technologie kommt dabei zum Schluss. Zuerst analysieren wir daher gemeinsam Ihre fachlichen Bedürfnisse und unternehmensspezifischen Hintergrund. Erst im Anschluss bringen wir unser technisches Tiefen- und prozessuales Breitenwissen ein, um Ihre Geschäftsprozesse bestmöglich umzusetzen. Unsere Philosophie ist darauf ausgerichtet Qualitätskriterien für modernes, effektives und angenehmes Arbeiten umzusetzen, damit wir in einem kontinuierlichen Verbesserungsprozess die Weichen sukzessive auf nachhaltige, erfolgreiche Veränderung stellen. Regelmäßig bedarf es dafür nicht nur das Wissen darüber, was funktioniert, sondern eben auch was nicht funktioniert.',
       image: {
-        src: '/img/about/team-up.webp',
+        src: '/img/about/team-waling-in-the-nature.webp',
         alt: 'article'
       }
     },
@@ -30,7 +30,7 @@ export default component$(() => {
       heading: 'Uns prägt Faszination',
       text: 'Speziellen Wert legen wir deshalb auf die Auswahl unserer Mitarbeiter. Wir wissen, dass Faszination und Leidenschaft zu Höchstleistungen motivieren und wo diese sind, ist Kollegialität auch nicht weit. Die gute Zusammenarbeit hat besondere Vorteile für Sie: Lösungen können entlang der gesamten Wertschöpfungskette entwickelt werden. Der Übergang von der Prozessanalyse über das Bereitstellen der zuverlässigen Infrastruktur bis hin zur individuellen Softwareentwicklung geschieht nahtlos und liefert Ihnen einen echten Mehrwert.',
       image: {
-        src: '/img/about/it-fascination.webp',
+        src: '/img/about/team-reviewing-a-solution.webp',
         alt: 'article'
       }
     }
