94758c785c917526c1b8c311ecc35f8eb2aeb2b4
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-04-25 11:19:41 +0200

    fix(legal): display list items correctly

--- src/routes/legal/layout.tsx
+++ src/routes/legal/layout.tsx
@@ -1,6 +1,9 @@
-import { component$, Slot } from '@builder.io/qwik';
+import { component$, Slot, useStyles$ } from '@builder.io/qwik';
+import styles from './layout.css?inline';
 
 export default component$(() => {
+  useStyles$(styles);
+
   return (
     <div class='container mx-auto mt-8'>
       <Slot></Slot>
