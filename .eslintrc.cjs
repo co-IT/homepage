49ad12737ac550836662bad1a4c5d89b989dfc48
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-05-11 14:39:11 +0200

    chore(eslint): lint deprecated api-usages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,7 +20,7 @@ module.exports = {
       jsx: true
     }
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'deprecation'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
@@ -36,6 +36,7 @@ module.exports = {
     'no-case-declarations': 'off',
     'no-console': 'off',
     '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/consistent-type-imports': 'warn'
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    'deprecation/deprecation': 'warn'
   }
 };
