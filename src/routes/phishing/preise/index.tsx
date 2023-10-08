6aa29b7f12040d208c3e1ce12887d7e76fe4c1e8
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-10-08 21:58:01 +0200

    feat(phishing): add detailed information

--- src/routes/phishing/preise/index.tsx
+++ src/routes/phishing/preise/index.tsx
@@ -2,44 +2,126 @@ import { component$, useStyles$, useVisibleTask$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 import { CheckIcon } from '../../../components/icons';
+import { InfoPopover } from '../../../components/info-popover/info-popover';
 import style from './styles.css?inline';
 
 type PhisingFeature = {
   text: string;
-  detail: string;
+  detail?: string;
 };
 
 const starterFeatures: PhisingFeature[] = [
-  { text: '1 E-Mail / Monat / Anwender', detail: '' },
-  { text: 'E-Mail-Support', detail: '' },
-  { text: 'Frequenz: 1 pro Monat', detail: '' },
-  { text: 'Sprachniveau: Akzeptabel', detail: '' }
+  {
+    text: 'Anzahl E-Mails: 12',
+    detail: 'Anzahl der Phising Mails pro Anwender und Jahr'
+  },
+  {
+    text: 'E-Mail-Support',
+    detail:
+      'Unsere Reaktionszeit wird an Werktagen innerhalb 48 Stunden in deutscher Sprache gewährleistet.'
+  },
+  { text: 'Frequenz: 1 pro Monat' },
+  {
+    text: 'Sprachniveau: Mittel',
+    detail:
+      'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
+  }
 ];
 
 const profiFeatures: PhisingFeature[] = [
-  { text: '1 E-Mail / Monat / Anwender', detail: '' },
-  { text: 'E-Mail-Support', detail: '' },
-  { text: 'Frequenz: 1 pro Monat', detail: '' },
-  { text: 'Sprachniveau: Hoch', detail: '' },
-  { text: 'Personalisierte Anrede', detail: '' },
-  { text: 'Authentische Absenderadressen', detail: '' },
-  { text: 'Inhalte: Microsoft', detail: '' }
+  {
+    text: 'Anzahl E-Mails: 24',
+    detail: 'Anzahl der Phising Mails pro Anwender und Jahr'
+  },
+  {
+    text: 'E-Mail-Support',
+    detail:
+      'Unsere Reaktionszeit wird an Werktagen innerhalb 48 Stunden in deutscher Sprache gewährleistet.'
+  },
+  {
+    text: 'Frequenz: je Monat (aufeinanderfolgende Tage)',
+    detail:
+      'Im Tarif "Profi" werden die zwei monatlichen E-Mails an aufeinanderfolgneden Tagen versendet.'
+  },
+  {
+    text: 'Sprachniveau: Hoch',
+    detail:
+      'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausibilität geachtet.'
+  },
+  {
+    text: 'Personalisierte Anrede',
+    detail:
+      'In einem Teil der Phishing-Mails wird auch personalisierte Ansprache verwendet, das heißt die E-Mails enthalten Anrede und Name des Anwenders. Damit werden Ihre Anwender noch besser für besonders gefährliche Phishings sensibilisiert.'
+  },
+  {
+    text: 'Authentische Absenderadressen',
+    detail:
+      'In einem Teil der Phishing-Mails werden passend zu deren Inhalten glaubwürdige Domains verwendet. Geht es in der Mail zum Beispiel um Urheberrechtsverstöße, so würde als Absender klage@kanzlei-abele.de verwendet werden.'
+  },
+  {
+    text: 'Inhalte: Microsoft',
+    detail:
+      'In einem Teil der Phising-Mails wird auf Produkte wie Outlook und Teams eingegangen. Besonders wenn Sie Microsoft-Produkte einsetzten, erhalten erfahrungsgemäß diese Phishing-Mails gesteigerte Aufmerksamkeit.'
+  }
 ];
 
 const expertFeatures: PhisingFeature[] = [
-  { text: '1 E-Mail / Monat / Anwender', detail: '' },
-  { text: 'E-Mail-Support', detail: '' },
-  { text: 'Frequenz: 1 pro Monat', detail: '' },
-  { text: 'Sprachniveau Hoch', detail: '' },
-  { text: 'Personalisierte Anrede', detail: '' },
-  { text: 'Authentische Absenderadressen', detail: '' },
-  { text: 'Inhalte: Microsoft', detail: '' },
-  { text: 'Inhalte: Zahlungsaufforderung', detail: '' },
-  { text: 'Anzeigename Cyber-Verantwortlicher', detail: '' },
-  { text: 'Englischsprachige Texte', detail: '' },
-  { text: 'Telefonischer Support', detail: '' },
-  { text: 'Einfluss auf Weiterentwicklung', detail: '' },
-  { text: 'Unternehmenszertifikat Phising', detail: '' }
+  {
+    text: 'Anzahl E-Mails: 52',
+    detail: 'Anzahl der Phising Mails pro Anwender und Jahr'
+  },
+  { text: 'E-Mail-Support' },
+  {
+    text: 'Frequenz: wöchentlich',
+    detail:
+      'Im Tarif "Experte" kommt pro Woche zu einer zufälligen Zeit die Phising-Mail.'
+  },
+  {
+    text: 'Sprachniveau Hoch',
+    detail:
+      'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausibilität geachtet.'
+  },
+  {
+    text: 'Personalisierte Anrede',
+    detail:
+      'In einem Teil der Phishing-Mails wird auch personalisierte Ansprache verwendet, das heißt die E-Mails enthalten Anrede und Name des Anwenders. Damit werden Ihre Anwender noch besser für besonders gefährliche Phishings sensibilisiert.'
+  },
+  {
+    text: 'Authentische Absenderadressen',
+    detail:
+      'In einem Teil der Phishing-Mails werden passend zu deren Inhalten glaubwürdige Domains verwendet. Geht es in der Mail zum Beispiel um Urheberrechtsverstöße, so würde als Absender klage@kanzlei-abele.de verwendet werden.'
+  },
+  {
+    text: 'Inhalte: Microsoft',
+    detail:
+      'In einem Teil der Phising-Mails wird auf Produkte wie Outlook und Teams eingegangen. Besonders wenn Sie Microsoft-Produkte einsetzten, erhalten erfahrungsgemäß diese Phishing-Mails gesteigerte Aufmerksamkeit.'
+  },
+  {
+    text: 'Inhalte: Zahlungsaufforderung',
+    detail:
+      'In einem Teil der Phishing-Mails wird der Anwender aufgefordert sich mit einer Zahlungsaufforderung auseinanderzusetzen. Zum einen adressieren Sie damit gezielt Ihr Rechnungswesen. Zum anderen erhalten erfahrungsgemäß diese Phising-Mails gesteigerte Aufmerksamkeit.'
+  },
+  {
+    text: 'Anzeigename Cyber-Verantwortlicher',
+    detail:
+      'In einem Teil der Phishing-Mails wird in der Grußformel der Name des Cyber-Verantwortlichen Ihres Unternehmens genannt. Zudem wird dieser Name als Anzeigename dargestellt. Damit werden Ihre Anwender auch für Phishing-Mails innerhalb Ihres Unternehmens sensibilisiert.'
+  },
+  {
+    text: 'Englischsprachige Texte',
+    detail:
+      'Zusätzlich zur Standardsprache Deutsch erhalten Ihre Anwender auf Mails in englischer Sprache. Das ist zum Beispiel hilfreich, wenn Sie regelmäßig mit internationalen Kontakten kommunizieren.'
+  },
+  {
+    text: 'Telefonischer Support',
+    detail:
+      'Unsere Erreichbarkeit ist an Werktagen von 9 - 16 Uhr in deutscher Sprache gewährleistet.'
+  },
+  {
+    text: 'Einfluss auf Weiterentwicklung',
+    detail:
+      'Nehmen Sie Einfluss auf die weiter Produktentwicklung und stimmen Sie über neue Funktionalitäten ab.'
+  },
+  { text: 'Unternehmenszertifikat Phising' }
 ];
 
 export default component$(() => {
@@ -235,9 +317,13 @@ export default component$(() => {
             <ul>
               {starterFeatures.map((feature, key) => {
                 return (
-                  <li key={key} class='align-center flex gap-1 p-2'>
+                  <li
+                    key={key}
+                    class='align-center grid grid-cols-[auto_auto_1fr] justify-items-end gap-2 pb-4'
+                  >
                     <CheckIcon />
                     <span>{feature.text}</span>
+                    {feature.detail && <InfoPopover text={feature.detail} />}
                   </li>
                 );
               })}
@@ -258,9 +344,13 @@ export default component$(() => {
             <ul>
               {profiFeatures.map((feature, key) => {
                 return (
-                  <li key={key} class='flex gap-1 p-2'>
+                  <li
+                    key={key}
+                    class='align-center grid grid-cols-[auto_auto_1fr] justify-items-end gap-2 pb-4'
+                  >
                     <CheckIcon />
                     <span>{feature.text}</span>
+                    {feature.detail && <InfoPopover text={feature.detail} />}
                   </li>
                 );
               })}
@@ -282,9 +372,13 @@ export default component$(() => {
             <ul>
               {expertFeatures.map((feature, key) => {
                 return (
-                  <li key={key} class='align-center flex gap-1 p-2'>
+                  <li
+                    key={key}
+                    class='align-center grid grid-cols-[auto_auto_1fr] justify-items-end gap-2 pb-4'
+                  >
                     <CheckIcon />
                     <span>{feature.text}</span>
+                    {feature.detail && <InfoPopover text={feature.detail} />}
                   </li>
                 );
               })}
