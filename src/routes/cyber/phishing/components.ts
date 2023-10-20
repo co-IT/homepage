09f2ad58d3359ce552ce6001fb9afd4af3ebf725
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-10-20 10:34:52 +0200

    feat(cyber): provide phising fake site

---
+++ src/routes/cyber/phishing/components.ts
@@ -0,0 +1,5 @@
+export { VideoOverlapLandscape } from '~/components/content-overlap';
+export { HeadingArticle } from '~/components/heading-article';
+export { HeadingSegmentSecondary } from '~/components/heading-segment';
+export { LinkCallToAction } from '~/components/link-call-to-action';
+export { SectionArea } from '~/components/section-area';
