b2e8c56b9b055372cd15239042f8d15e5f773c85
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-10-20 11:12:09 +0200

    feat(cyber): add video section for "Cyber-Verantwortliche" (#198)

---
+++ public/markdown/youtube-video-descriptions/MTtJwd7UrTA.md
@@ -0,0 +1,1 @@
+In diesem Video zeigen wir Ihnen wie Sie die E-Mails und Phishing Simulationen aus unserem Cyber-Portal in der Sophos XGS Mail Protection freigeben können.
--- src/routes/cyber/cyber-portal/index.tsx
+++ src/routes/cyber/cyber-portal/index.tsx
@@ -6,6 +6,7 @@ import {
   useStyles$,
   useTask$
 } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
 
 import { BlackCloseIcon, CheckIcon } from '~/components/icons';
 import { InfoPopover } from '~/components/info-popover/info-popover';
@@ -15,6 +16,7 @@ import {
   ContentOverlapPortrait,
   VideoOverlapLandscape
 } from '~/components/content-overlap';
+
 import { HeadingArticle } from '~/components/heading-article';
 import {
   HeadingSegmentSecondary,
@@ -22,146 +24,20 @@ import {
 } from '~/components/heading-segment';
 import { LinkCallToAction } from '~/components/link-call-to-action';
 
-import type { DocumentHead } from '@builder.io/qwik-city';
-import { ShieldCheckmarkIcon } from '../../../components/icons/shield-checkmark-icon';
-import { StarIcon } from '../../../components/icons/star-icon';
-import { ThumbsUpIcon } from '../../../components/icons/thumbs-up-icon';
-import { Modal, ModalContent, ModalHeader } from '../../../components/qwik-ui';
+import { ShieldCheckmarkIcon } from '~/components/icons/shield-checkmark-icon';
+import { StarIcon } from '~/components/icons/star-icon';
+import { ThumbsUpIcon } from '~/components/icons/thumbs-up-icon';
+import { Modal, ModalContent, ModalHeader } from '~/components/qwik-ui';
+import { YouTubeVideoGrid } from '~/components/youtube';
+
+import {
+  cyberVerantwortlicheVideos,
+  expertFeatures,
+  profiFeatures,
+  starterFeatures
+} from './resources';
+
 import style from './styles.css?inline';
-
-type PhishingFeature = {
-  text: string;
-  detail?: string;
-};
-
-const starterFeatures: PhishingFeature[] = [
-  {
-    text: 'Anzahl E-Mails: 12',
-    detail: 'Anzahl der Phising Mails pro Anwender und Jahr'
-  },
-  {
-    text: 'E-Mail-Support',
-    detail:
-      'Unsere Reaktionszeit wird an Werktagen innerhalb 48 Stunden in deutscher Sprache gewährleistet.'
-  },
-  { text: 'Frequenz: 1 pro Monat' },
-  {
-    text: 'Sprachniveau: Mittel',
-    detail:
-      'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
-  },
-  {
-    text: 'Zahlweise: monatlich',
-    detail:
-      'Auch bei einer längeren Vertragslaufzeit als 1 Monat rechnen wir monatlich ab, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den einzelnen Zahlungsterminen pro Monat für die gesamte Vertragslaufzeit.'
-  }
-];
-
-const profiFeatures: PhishingFeature[] = [
-  {
-    text: 'Anzahl E-Mails: 24',
-    detail: 'Anzahl der Phising Mails pro Anwender und Jahr'
-  },
-  {
-    text: 'E-Mail-Support',
-    detail:
-      'Unsere Reaktionszeit wird an Werktagen innerhalb 48 Stunden in deutscher Sprache gewährleistet.'
-  },
-  {
-    text: 'Frequenz: je Monat',
-    detail:
-      'Im Tarif "Profi" werden die zwei monatlichen E-Mails an aufeinanderfolgneden Tagen versendet.'
-  },
-  {
-    text: 'Sprachniveau: Hoch',
-    detail:
-      'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausibilität geachtet.'
-  },
-  {
-    text: 'Zahlweise: monatlich',
-    detail:
-      'Auch bei einer längeren Vertragslaufzeit als 1 Monat rechnen wir monatlich ab, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den einzelnen Zahlungsterminen pro Monat für die gesamte Vertragslaufzeit.'
-  },
-  {
-    text: 'Personalisierte Anrede',
-    detail:
-      'In einem Teil der Phishing-Mails wird auch personalisierte Ansprache verwendet, das heißt die E-Mails enthalten Anrede und Name des Anwenders. Damit werden Ihre Anwender noch besser für besonders gefährliche Phishings sensibilisiert.'
-  },
-  {
-    text: 'Authentische Absenderadressen',
-    detail:
-      'In einem Teil der Phishing-Mails werden passend zu deren Inhalten glaubwürdige Domains verwendet. Geht es in der Mail zum Beispiel um Urheberrechtsverstöße, so würde als Absender klage@kanzlei-abele.de verwendet werden.'
-  },
-  {
-    text: 'Inhalte: Microsoft',
-    detail:
-      'In einem Teil der Phising-Mails wird auf Produkte wie Outlook und Teams eingegangen. Besonders wenn Sie Microsoft-Produkte einsetzten, erhalten erfahrungsgemäß diese Phishing-Mails gesteigerte Aufmerksamkeit.'
-  }
-];
-
-const expertFeatures: PhishingFeature[] = [
-  {
-    text: 'Anzahl E-Mails: 52',
-    detail: 'Anzahl der Phising Mails pro Anwender und Jahr'
-  },
-  { text: 'E-Mail-Support' },
-  {
-    text: 'Frequenz: wöchentlich',
-    detail:
-      'Im Tarif "Experte" kommt pro Woche zu einer zufälligen Zeit die Phising-Mail.'
-  },
-  {
-    text: 'Sprachniveau Hoch',
-    detail:
-      'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausibilität geachtet.'
-  },
-  {
-    text: 'Zahlweise: monatlich',
-    detail:
-      'Auch bei einer längeren Vertragslaufzeit als 1 Monat rechnen wir monatlich ab, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den einzelnen Zahlungsterminen pro Monat für die gesamte Vertragslaufzeit.'
-  },
-  {
-    text: 'Personalisierte Anrede',
-    detail:
-      'In einem Teil der Phishing-Mails wird auch personalisierte Ansprache verwendet, das heißt die E-Mails enthalten Anrede und Name des Anwenders. Damit werden Ihre Anwender noch besser für besonders gefährliche Phishings sensibilisiert.'
-  },
-  {
-    text: 'Authentische Absenderadressen',
-    detail:
-      'In einem Teil der Phishing-Mails werden passend zu deren Inhalten glaubwürdige Domains verwendet. Geht es in der Mail zum Beispiel um Urheberrechtsverstöße, so würde als Absender klage@kanzlei-abele.de verwendet werden.'
-  },
-  {
-    text: 'Inhalte: Microsoft',
-    detail:
-      'In einem Teil der Phising-Mails wird auf Produkte wie Outlook und Teams eingegangen. Besonders wenn Sie Microsoft-Produkte einsetzten, erhalten erfahrungsgemäß diese Phishing-Mails gesteigerte Aufmerksamkeit.'
-  },
-  {
-    text: 'Inhalte: Zahlungsaufforderung',
-    detail:
-      'In einem Teil der Phishing-Mails wird der Anwender aufgefordert sich mit einer Zahlungsaufforderung auseinanderzusetzen. Zum einen adressieren Sie damit gezielt Ihr Rechnungswesen. Zum anderen erhalten erfahrungsgemäß diese Phising-Mails gesteigerte Aufmerksamkeit.'
-  },
-  {
-    text: 'Anzeigename Cyber-Verantwortlicher',
-    detail:
-      'In einem Teil der Phishing-Mails wird in der Grußformel der Name des Cyber-Verantwortlichen Ihres Unternehmens genannt. Zudem wird dieser Name als Anzeigename dargestellt. Damit werden Ihre Anwender auch für Phishing-Mails innerhalb Ihres Unternehmens sensibilisiert.'
-  },
-  {
-    text: 'Englischsprachige Texte',
-    detail:
-      'Zusätzlich zur Standardsprache Deutsch erhalten Ihre Anwender auf Mails in englischer Sprache. Das ist zum Beispiel hilfreich, wenn Sie regelmäßig mit internationalen Kontakten kommunizieren.'
-  },
-  {
-    text: 'Telefonischer Support',
-    detail:
-      'Unsere Erreichbarkeit ist an Werktagen von 9 - 16 Uhr in deutscher Sprache gewährleistet.'
-  },
-  {
-    text: 'Einfluss auf Weiterentwicklung',
-    detail:
-      'Nehmen Sie Einfluss auf die weiter Produktentwicklung und stimmen Sie über neue Funktionalitäten ab.'
-  },
-  { text: 'Unternehmenszertifikat Phising' }
-];
 
 export default component$(() => {
   useStyles$(style);
@@ -705,6 +581,15 @@ export default component$(() => {
             Erstberatung vereinbaren
           </LinkCallToAction>
         </div>
+      </SectionArea>
+
+      <SectionArea>
+        <div class='mx-auto mb-4 h-1 w-10 bg-primary'></div>
+        <h2 class='text-secondary mb-14 text-center text-4xl font-bold'>
+          WICHTIG - Für Cyber-Verantwortliche
+        </h2>
+
+        <YouTubeVideoGrid videos={cyberVerantwortlicheVideos} />
       </SectionArea>
     </>
   );
