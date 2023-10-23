6d9516f255c7b15e6ccbf65f3072850e032e2072
Uli Armbruster <uli.armbruster@co-IT.eu>
2023-10-23 15:30:33 +0200

    chore(cyber-portal): Texte für Profi und Experte finalisiert

--- src/routes/cyber/cyber-portal/resources/features.ts
+++ src/routes/cyber/cyber-portal/resources/features.ts
@@ -5,6 +5,96 @@ type ProductFeature = {
 
 export const starterFeatures: ProductFeature[] = [
   {
+    text: 'E-Mail-Support',
+    detail:'Unsere Reaktionszeiten sind in deutscher Sprache von Montag-Freitag zw. 9-16 Uhr und innerhalb von 48 Stunden.'
+  },{
+    text: 'Zahlweise: monatlich',
+    detail:'Auch bei Vertragslaufzeit länger als einen Monat erfolgt die Rechnungslegung monatlich, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den monatlichen Zahlungsterminen für die gesamte Vertragslaufzeit.'
+  }{
+    text: 'Anzahl E-Mails: 12',
+    detail: 'Anzahl der Phishing Simulationen pro Empfänger und Jahr.'
+  },{ 
+    text: 'Frequenz: 30 Tage',
+    detail: 'Pro Monat erhalten Empfänger eine Phishing Simulation.'
+  },{ 
+    text: 'Versandzeit: Gleichzeitig',
+    detail: 'Alle Phishing Simulationen werden zur gleichen Zeit an alle Empfänger verschickt.'
+  },{
+    text: 'Sprachniveau: Hoch',
+    detail: 'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
+  },{
+    text: 'Absender: Authentisch',
+    detail: 'Authentische Absenderadressen sind z.B. Domains wie "bundesgerichtshof-karlsruhe.de" oder "bund-und-laender.online", wohingegen eine Domäne wie "why-how-what.info" weniger echt wirkt.'
+  },{
+    text: 'Anrede: Personalisiert',
+    detail: 'In den Phishing Simulationen wird der Name des Empfängers in der Anrede verwendet, also z.B. "Sehr geehrter Herr Tim Kater".'
+  },{
+    text: 'Unternehmensname',
+    detail: 'In den Phishing Simulationen wird Ihr Unternehmen referenziert, z.B. indem in einer Simulation eine "Kununu"-Bewertung zu Ihrer Firma nachgestellt wird.'
+  },{
+    text: '"Attack Pack": Behörden',
+    detail: 'Arbeitsrecht, Elternzeit, Unfallversicherung: Diese Inhalte interessieren praktisch jeden und haben eine hohe Klickquote.'
+  },{
+    text: 'Abstimmen über neue Funktionen',
+    detail: 'Ihre Stimme zählt: Sie können regelmäßig über die Priorisierung von neuen Funktionen mit abstimmen.'
+  }
+];
+
+export const profiFeatures: ProductFeature[] = [
+  {
+    text: 'E-Mail-Support',
+    detail:'Unsere Reaktionszeiten sind in deutscher Sprache von Montag-Freitag zw. 9-16 Uhr und innerhalb von 48 Stunden.'
+  },{
+    text: 'Zahlweise: monatlich',
+    detail:'Auch bei Vertragslaufzeit länger als einen Monat erfolgt die Rechnungslegung monatlich, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den monatlichen Zahlungsterminen für die gesamte Vertragslaufzeit.'
+  },{
+    text: 'Anzahl E-Mails: 24',
+    detail: 'Anzahl der Phishing Simulationen pro Empfänger und Jahr.'
+  },{ 
+    text: 'Frequenz: 14 Tage',
+    detail: 'Alle zwei Wochen erhalten Empfänger eine Phishing Simulation.'
+  },{ 
+    text: 'Versandzeit: Gleichzeitig',
+    detail: 'Alle Phishing Simulationen werden zur gleichen Zeit an alle Empfänger verschickt.'
+  },{
+    text: 'Sprachniveau: Hoch',
+    detail: 'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
+  },{
+    text: 'Absender: Authentisch',
+    detail: 'Authentische Absenderadressen sind z.B. Domains wie "bundesgerichtshof-karlsruhe.de" oder "bund-und-laender.online", wohingegen eine Domäne wie "why-how-what.info" weniger echt wirkt.'
+  },{
+    text: 'Anrede: Personalisiert',
+    detail: 'In den Phishing Simulationen wird der Name des Empfängers in der Anrede verwendet, also z.B. "Sehr geehrter Herr Tim Kater".'
+  },{
+    text: 'Unternehmensname',
+    detail: 'In den Phishing Simulationen wird Ihr Unternehmen referenziert, z.B. indem in einer Simulation eine "Kununu"-Bewertung zu Ihrer Firma nachgestellt wird.'
+  },{
+    text: '"Attack Pack": Behörden',
+    detail: 'Arbeitsrecht, Elternzeit, Unfallversicherung: Diese Inhalte sind für jeden interessant und haben eine hohe Klickquote.'
+  },{
+    text: 'Abstimmung über neue Funktionen',
+    detail: 'Ihre Stimme zählt: Sie können regelmäßig über die Priorisierung von neuen Funktionen abstimmen.'
+  },{
+    text: 'Abteilungsname',
+    detail: 'In den Phishing Simulationen wird die Abteilung des Empfängers referenziert, z.B. indem eine "Datenschutzbefragung" im "Einkauf" simuliert wird.'
+  },{
+    text: 'Cyber-Verantwortlicher',
+    detail: 'In den Phishing Simulationen wird der Name des Cyber-Verantwortlichen angedruckt, z.B. dass "Reinhold Müller" via OneDrive eine Datei mit dem Empfänger geteilt hat.'
+  },{
+    text: '"Attack Pack": Microsoft',
+    detail: 'Microsoft verschickt regelmäßig Nachrichten zu Produkten wie Teams und Outlook. In diesen Simulationen versenden wir entsprechende Kopien.'
+  }
+];
+
+export const expertFeatures: ProductFeature[] = [
+  {
+    text: 'E-Mail-Support',
+    detail:'Unsere Reaktionszeiten sind in deutscher Sprache von Montag-Freitag zw. 9-16 Uhr und innerhalb von 48 Stunden.'
+  },{
+    text: 'Zahlweise: monatlich',
+    detail:'Auch bei Vertragslaufzeit länger als einen Monat erfolgt die Rechnungslegung monatlich, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den monatlichen Zahlungsterminen für die gesamte Vertragslaufzeit.'
+  },
+  {
     text: 'Anzahl E-Mails: 12',
     detail: 'Anzahl der Phishing Simulationen pro Empfänger und Jahr.'
   },{ 
@@ -12,7 +102,7 @@ export const starterFeatures: ProductFeature[] = [
     detail: 'Pro Monat erhalten Ihre Anwender eine Phishing-Simulation.'
   },{ 
     text: 'Versandzeit: Gleichzeitig',
-    detail: 'Alle Phishing Simulationen gehen zur gleichen Zeit an die Anwender raus.'
+    detail: 'Alle Phishing Simulationen werden zur gleichen Zeit an die Anwender verschickt.'
   },{
     text: 'Sprachniveau: Mittel',
     detail: 'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
@@ -26,146 +116,34 @@ export const starterFeatures: ProductFeature[] = [
     text: 'Unternehmensname: Ja',
     detail: 'In den Phishing Simulationen wird Ihr Unternehmen referenziert, z.B. indem in einer Simulation eine Kununu-Bewertung zu Ihrer Firma nachgestellt wird.'
   },{
-    text: 'Abteilungsname: Nein',
-    detail: 'In den Phishing Simulationen wird die Abteilungs des Empfängers referenziert, z.B. indem eine Datenschutzbefragung im Einkauf simuliert wird.'
-  },{
-    text: 'Cyber-Verantwortlicher: Nein',
-    detail: 'In den Phishing Simulationen wird der Name des Cyber-Verantwortlichen angedruckt, z.B. dass dieser eine Datei mit dem Empfänger teilt.'
-  },{
-    text: 'Englischsprachige Texte',
-    detail: 'Einige Phishing Simulationen werden in englischer Sprache verfasst. Das ist für Unternehmen hilfreich, die regelmäßig mit dem Ausland korrespondieren.'
-  },{
-    text: 'Zahlungsaufforderungen: Nein',
-    detail: 'Speziell bei betrügerischen Zahlungsaufforderungen kann es schnell zu finanziellen Schäden kommen. Mit diesen Angriffen adressieren wir vor allem das Rechnungswesen und den Einkauf.'
-  },{
-    text: 'Microsoft Produkte: Nein',
-    detail: 'Microsoft verschickt regelmäßig Nachrichten zu Produkten wie Teams und Outlook. In diesen Simulationen versenden wir entsprechende Kopien.'
-  },{
-    text: 'Behörden Updates: Ja',
+    text: 'Attack Pack: Behörden',
     detail: 'Arbeitsrecht, Elternzeit, Unfallversicherung: Diese Inhalte interessieren praktisch jeden und haben eine hohe Klickquote.'
   },{
-    text: 'Eigene Inhalte: Nein',
-    detail: 'Arbeitsrecht, Elternzeit, Unfallversicherung: Diese Inhalte interessieren praktisch jeden und haben eine hohe Klickquote.'
-  },{
-    text: 'Abstimmung über neue Funktionen: Ja',
+    text: 'Abstimmung über neue Funktionen',
     detail: 'Ihre Stimme zählt: Sie können regelmäßig über die Priorisierung von neuen Funktionen mit abstimmen.'
   },{
-    text: 'Vorschlagen von Funktionen: Ja',
-    detail: 'Teilen Sie uns Ihre Ideen unserem Entwicklungsteam mit.'
+    text: 'Abteilungsname',
+    detail: 'In den Phishing Simulationen wird die Abteilung des Empfängers referenziert, z.B. indem eine "Datenschutzbefragung" im "Einkauf" simuliert wird.'
   },{
-    text: 'E-Mail-Support: Ja',
-    detail:'Unsere Reaktionszeit wird an Werktagen innerhalb 48 Stunden in deutscher Sprache gewährleistet.'
+    text: 'Cyber-Verantwortlicher',
+    detail: 'In den Phishing Simulationen wird der Name des Cyber-Verantwortlichen angedruckt, z.B. dass "Reinhold Müller" via OneDrive eine Datei mit dem Empfänger geteilt hat.'
   },{
-    text: 'telefonischer Support: Nein',
-    detail:'Montag - Freitag zw. 9-16 Uhr mit Ausnahme gesetzlicher Feiertage in Baden-Württemberg.'
-  },{
-    text: 'Zahlweise: monatlich',
-    detail:'Auch bei Vertragslaufzeit länger als einen Monat erfolgt die Rechnungslegung monatlich, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den monatlichen Zahlungsterminen für die gesamte Vertragslaufzeit.'
-  }
-];
-
-export const profiFeatures: ProductFeature[] = [
-  {
-    text: 'Anzahl E-Mails: 24',
-    detail: 'Anzahl der Phising Mails pro Anwender und Jahr'
-  },
-  {
-    text: 'E-Mail-Support',
-    detail:
-      'Unsere Reaktionszeit wird an Werktagen innerhalb 48 Stunden in deutscher Sprache gewährleistet.'
-  },
-  {
-    text: 'Frequenz: 14 Tage',
-    detail:
-      'Im Rhytmus von 2 Wochen erhalten Ihre Anwender eine Phishing-Simulation.'
-  },
-  {
-    text: 'Sprachniveau: Hoch',
-    detail:
-      'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausibilität geachtet.'
-  },
-  {
-    text: 'Zahlweise: monatlich',
-    detail:
-      'Auch bei einer längeren Vertragslaufzeit als 1 Monat rechnen wir monatlich ab, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den einzelnen Zahlungsterminen pro Monat für die gesamte Vertragslaufzeit.'
-  },
-  {
-    text: 'Personalisierte Anrede',
-    detail:
-      'In einem Teil der Phishing-Mails wird auch personalisierte Ansprache verwendet, das heißt die E-Mails enthalten Anrede und Name des Anwenders. Damit werden Ihre Anwender noch besser für besonders gefährliche Phishings sensibilisiert.'
-  },
-  {
-    text: 'Authentische Absenderadressen',
-    detail:
-      'In einem Teil der Phishing-Mails werden passend zu deren Inhalten glaubwürdige Domains verwendet. Geht es in der Mail zum Beispiel um Urheberrechtsverstöße, so würde als Absender klage@kanzlei-abele.de verwendet werden.'
-  },
-  {
-    text: 'Inhalte: Microsoft',
-    detail:
-      'In einem Teil der Phising-Mails wird auf Produkte wie Outlook und Teams eingegangen. Besonders wenn Sie Microsoft-Produkte einsetzten, erhalten erfahrungsgemäß diese Phishing-Mails gesteigerte Aufmerksamkeit.'
-  }
-];
-
-export const expertFeatures: ProductFeature[] = [
-  {
-    text: 'Anzahl E-Mails: 52',
-    detail: 'Anzahl der Phising Mails pro Anwender und Jahr'
-  },
-  { text: 'E-Mail-Support' },
-  {
-    text: 'Frequenz: wöchentlich',
-    detail:
-      'Im Tarif "Experte" kommt pro Woche zu einer zufälligen Zeit die Phising-Mail.'
-  },
-  {
-    text: 'Sprachniveau Hoch',
-    detail:
-      'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausibilität geachtet.'
-  },
-  {
-    text: 'Zahlweise: monatlich',
-    detail:
-      'Auch bei einer längeren Vertragslaufzeit als 1 Monat rechnen wir monatlich ab, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den einzelnen Zahlungsterminen pro Monat für die gesamte Vertragslaufzeit.'
-  },
-  {
-    text: 'Personalisierte Anrede',
-    detail:
-      'In einem Teil der Phishing-Mails wird auch personalisierte Ansprache verwendet, das heißt die E-Mails enthalten Anrede und Name des Anwenders. Damit werden Ihre Anwender noch besser für besonders gefährliche Phishings sensibilisiert.'
-  },
-  {
-    text: 'Authentische Absenderadressen',
-    detail:
-      'In einem Teil der Phishing-Mails werden passend zu deren Inhalten glaubwürdige Domains verwendet. Geht es in der Mail zum Beispiel um Urheberrechtsverstöße, so würde als Absender klage@kanzlei-abele.de verwendet werden.'
-  },
-  {
-    text: 'Inhalte: Microsoft',
-    detail:
-      'In einem Teil der Phising-Mails wird auf Produkte wie Outlook und Teams eingegangen. Besonders wenn Sie Microsoft-Produkte einsetzten, erhalten erfahrungsgemäß diese Phishing-Mails gesteigerte Aufmerksamkeit.'
-  },
-  {
-    text: 'Inhalte: Zahlungsaufforderung',
-    detail:
-      'In einem Teil der Phishing-Mails wird der Anwender aufgefordert sich mit einer Zahlungsaufforderung auseinanderzusetzen. Zum einen adressieren Sie damit gezielt Ihr Rechnungswesen. Zum anderen erhalten erfahrungsgemäß diese Phising-Mails gesteigerte Aufmerksamkeit.'
-  },
-  {
-    text: 'Anzeigename Cyber-Verantwortlicher',
-    detail:
-      'In einem Teil der Phishing-Mails wird in der Grußformel der Name des Cyber-Verantwortlichen Ihres Unternehmens genannt. Zudem wird dieser Name als Anzeigename dargestellt. Damit werden Ihre Anwender auch für Phishing-Mails innerhalb Ihres Unternehmens sensibilisiert.'
-  },
-  {
     text: 'Englischsprachige Texte',
-    detail:
-      'Zusätzlich zur Standardsprache Deutsch erhalten Ihre Anwender auf Mails in englischer Sprache. Das ist zum Beispiel hilfreich, wenn Sie regelmäßig mit internationalen Kontakten kommunizieren.'
-  },
-  {
-    text: 'Telefonischer Support',
-    detail:
-      'Unsere Erreichbarkeit ist an Werktagen von 9 - 16 Uhr in deutscher Sprache gewährleistet.'
-  },
-  {
-    text: 'Einfluss auf Weiterentwicklung',
-    detail:
-      'Nehmen Sie Einfluss auf die weiter Produktentwicklung und stimmen Sie über neue Funktionalitäten ab.'
-  },
-  { text: 'Unternehmenszertifikat Phising' }
+    detail: 'Einige Phishing Simulationen werden in englischer Sprache verfasst. Das hilft  Unternehmen, die regelmäßig mit dem Ausland korrespondieren.'
+  },{
+    text: '"Attack Pack": Microsoft',
+    detail: 'Microsoft verschickt regelmäßig Nachrichten zu Produkten wie Teams und Outlook. In diesen Simulationen versenden wir entsprechende Kopien.'
+  },{
+    text: '"Attack Pack": Banking',
+    detail: 'Speziell bei betrügerischen Zahlungsaufforderungen kann es schnell zu finanziellen Schäden kommen. Mit diesen Simulationen adressieren sollen Rechnungswesen und Einkauf gesondert sensibilisert werden.'
+  },{
+    text: 'Eigene Inhalte',
+    detail: 'Bei einer entsprechend großen Zahl an Empfänger können wir für Sie gezielt Inhalte einsteuern. Sprechen Sie uns darauf an.'
+  },{
+    text: 'Funktionen vorschlagen',
+    detail: 'Teilen Sie Ihre Ideen unserem Entwicklungsteam mit.'
+  },{
+    text: 'telefonischer Support',
+    detail:'Montag - Freitag zw. 9-16 Uhr mit Ausnahme gesetzlicher Feiertage in Baden-Württemberg.'
+  }
 ];
