e60abfaeeabf92f4803d9855660c5724ffd01a2c
Uli Armbruster <uli.armbruster@co-IT.eu>
2023-10-23 15:30:33 +0200

    chore(cyber-portal): Texte für Profi und Experte finalisiert

--- src/routes/cyber/cyber-portal/resources/features.ts
+++ src/routes/cyber/cyber-portal/resources/features.ts
@@ -10,10 +10,10 @@ export const starterFeatures: ProductFeature[] = [
   },{
     text: 'Zahlweise: monatlich',
     detail:'Auch bei Vertragslaufzeit länger als einen Monat erfolgt die Rechnungslegung monatlich, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den monatlichen Zahlungsterminen für die gesamte Vertragslaufzeit.'
-  }{
+  },{
     text: 'Anzahl E-Mails: 12',
     detail: 'Anzahl der Phishing Simulationen pro Empfänger und Jahr.'
-  },{ 
+  },{
     text: 'Frequenz: 30 Tage',
     detail: 'Pro Monat erhalten Empfänger eine Phishing Simulation.'
   },{ 
@@ -55,7 +55,7 @@ export const profiFeatures: ProductFeature[] = [
     detail: 'Alle zwei Wochen erhalten Empfänger eine Phishing Simulation.'
   },{ 
     text: 'Versandzeit: Gleichzeitig',
-    detail: 'Alle Phishing Simulationen werden zur gleichen Zeit an alle Empfänger verschickt.'
+    detail: 'Alle Phishing Simulationen werden gleichzeitig an alle Empfänger verschickt.'
   },{
     text: 'Sprachniveau: Hoch',
     detail: 'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
@@ -93,34 +93,33 @@ export const expertFeatures: ProductFeature[] = [
   },{
     text: 'Zahlweise: monatlich',
     detail:'Auch bei Vertragslaufzeit länger als einen Monat erfolgt die Rechnungslegung monatlich, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den monatlichen Zahlungsterminen für die gesamte Vertragslaufzeit.'
-  },
-  {
-    text: 'Anzahl E-Mails: 12',
-    detail: 'Anzahl der Phishing Simulationen pro Empfänger und Jahr.'
-  },{ 
-    text: 'Frequenz: 30 Tage',
-    detail: 'Pro Monat erhalten Ihre Anwender eine Phishing-Simulation.'
-  },{ 
-    text: 'Versandzeit: Gleichzeitig',
-    detail: 'Alle Phishing Simulationen werden zur gleichen Zeit an die Anwender verschickt.'
   },{
-    text: 'Sprachniveau: Mittel',
+    text: 'Anzahl E-Mails: 52',
+    detail: 'Anzahl der Phishing Simulationen pro Empfänger und Jahr.'
+  },{
+    text: 'Frequenz: 7 Tage',
+    detail: 'Jede Woche erhalten die Empfänger eine Phishing-Simulation. Damit erreichen Sie maximale Sensibilisierung ohne den Empfänger zu häufig zu stören.'
+  },{
+    text: 'Versandzeit: Zufällig',
+    detail: 'Alle Phishing Simulationen werden pro Empfänger zufällig verschickt.'
+  },{
+    text: 'Sprachniveau: Hoch',
     detail: 'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
   },{
     text: 'Absender: Authentisch',
-    detail: 'Authentische Absenderadressen sind z.B. Domains wie bundesgerichtshof-karlsruhe.de oder bund-und-laender.online, wohingegen eine Domäne wie why-how-what.info weniger echt wirkt.'
+    detail: 'Authentische Absenderadressen sind z.B. Domains wie "bundesgerichtshof-karlsruhe.de" oder "bund-und-laender.online", wohingegen eine Domäne wie "why-how-what.info" weniger echt wirkt.'
   },{
     text: 'Anrede: Personalisiert',
     detail: 'In den Phishing Simulationen wird der Name des Empfängers in der Anrede verwendet, also z.B. "Sehr geehrter Herr Tim Kater".'
   },{
-    text: 'Unternehmensname: Ja',
-    detail: 'In den Phishing Simulationen wird Ihr Unternehmen referenziert, z.B. indem in einer Simulation eine Kununu-Bewertung zu Ihrer Firma nachgestellt wird.'
+    text: 'Unternehmensname',
+    detail: 'In den Phishing Simulationen wird Ihr Unternehmen referenziert, z.B. indem in einer Simulation eine "Kununu"-Bewertung zu Ihrer Firma nachgestellt wird.'
   },{
-    text: 'Attack Pack: Behörden',
-    detail: 'Arbeitsrecht, Elternzeit, Unfallversicherung: Diese Inhalte interessieren praktisch jeden und haben eine hohe Klickquote.'
+    text: '"Attack Pack": Behörden',
+    detail: 'Arbeitsrecht, Elternzeit, Unfallversicherung: Diese Inhalte sind für jeden interessant und haben eine hohe Klickquote.'
   },{
     text: 'Abstimmung über neue Funktionen',
-    detail: 'Ihre Stimme zählt: Sie können regelmäßig über die Priorisierung von neuen Funktionen mit abstimmen.'
+    detail: 'Ihre Stimme zählt: Sie können regelmäßig über die Priorisierung von neuen Funktionen abstimmen.'
   },{
     text: 'Abteilungsname',
     detail: 'In den Phishing Simulationen wird die Abteilung des Empfängers referenziert, z.B. indem eine "Datenschutzbefragung" im "Einkauf" simuliert wird.'
@@ -142,6 +141,9 @@ export const expertFeatures: ProductFeature[] = [
   },{
     text: 'Funktionen vorschlagen',
     detail: 'Teilen Sie Ihre Ideen unserem Entwicklungsteam mit.'
+  },{
+    text: 'Unternehmenszertifikat',
+    detail:'Nach 12 Monaten erhalten Sie ein auf Ihr Unternehmen ausgestelltes Zertifikat, das die kontinuierliche Sensibilisierung durch unsere Phishing Simulationen nachweist.'
   },{
     text: 'telefonischer Support',
     detail:'Montag - Freitag zw. 9-16 Uhr mit Ausnahme gesetzlicher Feiertage in Baden-Württemberg.'
