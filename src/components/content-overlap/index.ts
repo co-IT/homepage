18d72897167844973123eec2ed6914da4102523f
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-10-26 12:23:13 +0200

    feat(cyber): link ms forms instead of embedding it (#208)

--- src/components/content-overlap/index.ts
+++ src/components/content-overlap/index.ts
@@ -1,3 +1,2 @@
-export * from './content-overlap-landscape';
 export * from './content-overlap-portrait';
 export * from './video-overlap-landscape';
--- src/components/qwik-ui/modal/modal-content.tsx
+++
@@ -1,9 +0,0 @@
-import { QwikIntrinsicElements, Slot, component$ } from '@builder.io/qwik';
-
-export const ModalContent = component$((props: QwikIntrinsicElements['div']) => {
-  return (
-    <div {...props}>
-      <Slot />
-    </div>
-  );
-});
