e1ba352d39d07ff1ddf21f7483f4bb67f24d02c5
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-11-02 15:49:37 +0100

    feat(cyber): add basic package

--- src/routes/cyber/cyber-portal/index.tsx
+++ src/routes/cyber/cyber-portal/index.tsx
@@ -32,10 +32,13 @@ import {
   cyberVerantwortlicheVideos,
   expertFeatures,
   profiFeatures,
-  starterFeatures
+  starterFeatures,
+  wuerttembergischeFeatures
 } from './resources';
 
 import { LinkedHeading } from '../../../components/link-heading';
+import { PricePerUsePerMonth } from '../../../components/pricing/price-per-user-per-month';
+import { WuerttembergischeLogo } from '../../../components/wuerttembergische/logo';
 import style from './styles.css?inline';
 
 export default component$(() => {
@@ -330,9 +333,45 @@ export default component$(() => {
         </div>
 
         <div class='pricing-tiers justify-center'>
+          {/* Wuerttembergische Basic */}
           <div class='card grid items-start gap-8 shadow-md'>
             <div class='features'>
-              <h3 class='mb-4 flex items-center gap-4 text-3xl font-bold'>
+              <h3 class='mb-8 flex items-center gap-4 text-3xl font-bold text-[#48504F]'>
+                <WuerttembergischeLogo />
+                Basic
+              </h3>
+              <ul>
+                {wuerttembergischeFeatures.map((feature, key) => {
+                  return (
+                    <li
+                      key={key}
+                      class='align-center grid grid-cols-[auto_auto_1fr] justify-items-end gap-2 pb-4'
+                    >
+                      <CheckIcon />
+                      <span>{feature.text}</span>
+                      {feature.detail && <InfoPopover text={feature.detail} />}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div class='prices justify-end self-end'>
+              <a
+                href='https://www.wuerttembergische.de/geschaeftskunden/cyber-versicherung/'
+                target='_blank'
+                class='md rounded-[3.5rem] bg-[#f84914] p-4 text-center text-white transition-colors hover:bg-[#d33e11]'
+              >
+                Mehr zur Cyber-Versicherung
+              </a>
+              <br />
+              <small>Für Inhaber der Cyber-Versicherung inklusive</small>
+            </div>
+          </div>
+
+          {/* Starter */}
+          <div class='card grid items-start gap-8 shadow-md'>
+            <div class='features'>
+              <h3 class='mb-8 flex items-center gap-4 text-3xl font-bold'>
                 <ThumbsUpIcon />
                 Starter
               </h3>
@@ -351,21 +390,17 @@ export default component$(() => {
                 })}
               </ul>
             </div>
-            <div class='prices justify-end self-end'>
-              <span>Anwender / Monat</span>
-              <span class='price'>
-                {toEuro(pricingTier.starter.pricePerUserPerMonth)}
-              </span>
-              <span>Gesamt / Monat</span>
-              <span class='price'>
-                {toEuro(pricingTier.starter.pricePerMonth)}
-              </span>
-            </div>
+
+            <PricePerUsePerMonth
+              usersCount={+userCountSig.value}
+              pricePerUser={pricingTier.starter.pricePerUserPerMonth}
+            />
           </div>
 
+          {/* Profi */}
           <div class='card grid items-start gap-8 shadow-xl'>
             <div class='features'>
-              <h3 class='mb-4 flex items-center gap-4 text-3xl font-bold'>
+              <h3 class='mb-8 flex items-center gap-4 text-3xl font-bold'>
                 <StarIcon />
                 Profi
               </h3>
@@ -385,20 +420,16 @@ export default component$(() => {
               </ul>
             </div>
 
-            <div class='prices justify-end self-end '>
-              <span>Anwender / Monat</span>
-              <span class='price'>
-                {toEuro(pricingTier.professional.pricePerUserPerMonth)}
-              </span>
-              <span>Gesamt / Monat</span>
-              <span class='price'>
-                {toEuro(pricingTier.professional.pricePerYearMonth)}
-              </span>
-            </div>
+            <PricePerUsePerMonth
+              usersCount={+userCountSig.value}
+              pricePerUser={pricingTier.professional.pricePerUserPerMonth}
+            />
           </div>
+
+          {/* Expert */}
           <div class='card grid items-start gap-8 shadow-md' id='gold'>
             <div class='features'>
-              <h3 class='mb-4 flex items-center gap-4 text-3xl font-bold'>
+              <h3 class='mb-8 flex items-center gap-4 text-3xl font-bold'>
                 <ShieldCheckmarkIcon />
                 Experte
               </h3>
@@ -417,17 +448,10 @@ export default component$(() => {
                 })}
               </ul>
             </div>
-
-            <div class='prices justify-end self-end'>
-              <span>Anwender / Monat</span>
-              <span class='price'>
-                {toEuro(pricingTier.expert.pricePerUserPerMonth)}
-              </span>
-              <span>Gesamt / Monat</span>
-              <span class='price'>
-                {toEuro(pricingTier.expert.pricePerMonth)}
-              </span>
-            </div>
+            <PricePerUsePerMonth
+              usersCount={+userCountSig.value}
+              pricePerUser={pricingTier.expert.pricePerUserPerMonth}
+            />
           </div>
         </div>
       </Section>
--- src/routes/cyber/cyber-portal/resources/features.ts
+++ src/routes/cyber/cyber-portal/resources/features.ts
@@ -3,158 +3,291 @@ type ProductFeature = {
   detail?: string;
 };
 
+export const wuerttembergischeFeatures: ProductFeature[] = [
+  {
+    text: 'E-Mail-Support',
+    detail:
+      'Unsere Reaktionszeiten sind in deutscher Sprache von Montag-Freitag zw. 9-16 Uhr und innerhalb von 48 Stunden.'
+  },
+  {
+    text: 'Zahlweise: monatlich',
+    detail:
+      'Auch bei Vertragslaufzeit länger als einen Monat erfolgt die Rechnungslegung monatlich, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den monatlichen Zahlungsterminen für die gesamte Vertragslaufzeit.'
+  },
+  {
+    text: 'Klickrate: Hoch',
+    detail:
+      'Unsere Simulationen werden vorab immer von über 300 Anwendern in einer Testgruppe von unterschiedlichen Unternehmen einem Praxistest unterzogen: Dabei teilen wir die Klickquote in niedrig (>20%), mittel (>40%) und hoch (>60%) ein.'
+  },
+  {
+    text: 'Anzahl E-Mails: 12',
+    detail: 'Anzahl der Phishing Simulationen pro Empfänger und Jahr.'
+  },
+  {
+    text: 'Frequenz: 30 Tage',
+    detail: 'Pro Monat erhalten Empfänger eine Phishing Simulation.'
+  },
+  {
+    text: 'Versandzeit: Gleichzeitig',
+    detail:
+      'Alle Empfänger erhalten zur gleichen Zeit die gleiche Phishing Simulationen.'
+  },
+  {
+    text: 'Sprachniveau: Hoch',
+    detail:
+      'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
+  },
+  {
+    text: 'Absender: Authentisch',
+    detail:
+      'Authentische Absenderadressen sind z.B. Domains wie "bundesgerichtshof-karlsruhe.de" oder "bund-und-laender.online", wohingegen eine Domäne wie "why-how-what.info" weniger echt wirkt.'
+  },
+  {
+    text: 'Anrede: Personalisiert',
+    detail:
+      'In den Phishing Simulationen wird der Name des Empfängers in der Anrede verwendet, also z.B. "Sehr geehrter Herr Tim Kater".'
+  }
+];
+
 export const starterFeatures: ProductFeature[] = [
   {
     text: 'E-Mail-Support',
-    detail:'Unsere Reaktionszeiten sind in deutscher Sprache von Montag-Freitag zw. 9-16 Uhr und innerhalb von 48 Stunden.'
-  },{
+    detail:
+      'Unsere Reaktionszeiten sind in deutscher Sprache von Montag-Freitag zw. 9-16 Uhr und innerhalb von 48 Stunden.'
+  },
+  {
     text: 'Zahlweise: monatlich',
-    detail:'Auch bei Vertragslaufzeit länger als einen Monat erfolgt die Rechnungslegung monatlich, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den monatlichen Zahlungsterminen für die gesamte Vertragslaufzeit.'
-   },{
+    detail:
+      'Auch bei Vertragslaufzeit länger als einen Monat erfolgt die Rechnungslegung monatlich, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den monatlichen Zahlungsterminen für die gesamte Vertragslaufzeit.'
+  },
+  {
     text: 'Klickrate: Hoch',
-    detail:'Unsere Simulationen werden vorab immer von über 300 Anwendern in einer Testgruppe von unterschiedlichen Unternehmen einem Praxistest unterzogen: Dabei teilen wir die Klickquote in niedrig (>20%), mittel (>40%) und hoch (>60%) ein.'
-  },{
+    detail:
+      'Unsere Simulationen werden vorab immer von über 300 Anwendern in einer Testgruppe von unterschiedlichen Unternehmen einem Praxistest unterzogen: Dabei teilen wir die Klickquote in niedrig (>20%), mittel (>40%) und hoch (>60%) ein.'
+  },
+  {
     text: 'Anzahl E-Mails: 12',
     detail: 'Anzahl der Phishing Simulationen pro Empfänger und Jahr.'
-  },{
+  },
+  {
     text: 'Frequenz: 30 Tage',
     detail: 'Pro Monat erhalten Empfänger eine Phishing Simulation.'
-  },{ 
+  },
+  {
     text: 'Versandzeit: Gleichzeitig',
-    detail: 'Alle Empfänger erhalten zur gleichen Zeit die gleiche Phishing Simulationen.'
-  },{
+    detail:
+      'Alle Empfänger erhalten zur gleichen Zeit die gleiche Phishing Simulationen.'
+  },
+  {
     text: 'Sprachniveau: Hoch',
-    detail: 'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
-  },{
+    detail:
+      'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
+  },
+  {
     text: 'Absender: Authentisch',
-    detail: 'Authentische Absenderadressen sind z.B. Domains wie "bundesgerichtshof-karlsruhe.de" oder "bund-und-laender.online", wohingegen eine Domäne wie "why-how-what.info" weniger echt wirkt.'
-  },{
+    detail:
+      'Authentische Absenderadressen sind z.B. Domains wie "bundesgerichtshof-karlsruhe.de" oder "bund-und-laender.online", wohingegen eine Domäne wie "why-how-what.info" weniger echt wirkt.'
+  },
+  {
     text: 'Anrede: Personalisiert',
-    detail: 'In den Phishing Simulationen wird der Name des Empfängers in der Anrede verwendet, also z.B. "Sehr geehrter Herr Tim Kater".'
-  },{
+    detail:
+      'In den Phishing Simulationen wird der Name des Empfängers in der Anrede verwendet, also z.B. "Sehr geehrter Herr Tim Kater".'
+  },
+  {
     text: 'Unternehmensname',
-    detail: 'In den Phishing Simulationen wird Ihr Unternehmen referenziert, z.B. indem in einer Simulation eine "Kununu"-Bewertung zu Ihrer Firma nachgestellt wird.'
-  },{
+    detail:
+      'In den Phishing Simulationen wird Ihr Unternehmen referenziert, z.B. indem in einer Simulation eine "Kununu"-Bewertung zu Ihrer Firma nachgestellt wird.'
+  },
+  {
     text: '"Attack Pack": Behörden',
-    detail: 'Arbeitsrecht, Elternzeit, Unfallversicherung: Diese Inhalte interessieren praktisch jeden und haben eine hohe Klickquote.'
-  },{
+    detail:
+      'Arbeitsrecht, Elternzeit, Unfallversicherung: Diese Inhalte interessieren praktisch jeden und haben eine hohe Klickquote.'
+  },
+  {
     text: 'Abstimmen über neue Funktionen',
-    detail: 'Ihre Stimme zählt: Sie können regelmäßig über die Priorisierung von neuen Funktionen mit abstimmen.'
+    detail:
+      'Ihre Stimme zählt: Sie können regelmäßig über die Priorisierung von neuen Funktionen mit abstimmen.'
   }
 ];
 
 export const profiFeatures: ProductFeature[] = [
   {
     text: 'E-Mail-Support',
-    detail:'Unsere Reaktionszeiten sind in deutscher Sprache von Montag-Freitag zw. 9-16 Uhr und innerhalb von 48 Stunden.'
-  },{
+    detail:
+      'Unsere Reaktionszeiten sind in deutscher Sprache von Montag-Freitag zw. 9-16 Uhr und innerhalb von 48 Stunden.'
+  },
+  {
     text: 'Zahlweise: monatlich',
-    detail:'Auch bei Vertragslaufzeit länger als einen Monat erfolgt die Rechnungslegung monatlich, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den monatlichen Zahlungsterminen für die gesamte Vertragslaufzeit.'
-  },{
+    detail:
+      'Auch bei Vertragslaufzeit länger als einen Monat erfolgt die Rechnungslegung monatlich, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den monatlichen Zahlungsterminen für die gesamte Vertragslaufzeit.'
+  },
+  {
     text: 'Klickrate: Hoch',
-    detail:'Unsere Simulationen werden vorab immer von über 300 Anwendern in einer Testgruppe von unterschiedlichen Unternehmen einem Praxistest unterzogen: Dabei teilen wir die Klickquote in niedrig (>20%), mittel (>40%) und hoch (>60%) ein.'
-  },{
+    detail:
+      'Unsere Simulationen werden vorab immer von über 300 Anwendern in einer Testgruppe von unterschiedlichen Unternehmen einem Praxistest unterzogen: Dabei teilen wir die Klickquote in niedrig (>20%), mittel (>40%) und hoch (>60%) ein.'
+  },
+  {
     text: 'Anzahl E-Mails: 24',
     detail: 'Anzahl der Phishing Simulationen pro Empfänger und Jahr.'
-  },{ 
+  },
+  {
     text: 'Frequenz: 14 Tage',
     detail: 'Alle zwei Wochen erhalten Empfänger eine Phishing Simulation.'
-  },{ 
+  },
+  {
     text: 'Versandzeit: Gleichzeitig',
-    detail: 'Alle Empfänger erhalten zur gleichen Zeit die gleiche Phishing Simulationen.'
-  },{
+    detail:
+      'Alle Empfänger erhalten zur gleichen Zeit die gleiche Phishing Simulationen.'
+  },
+  {
     text: 'Sprachniveau: Hoch',
-    detail: 'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
-  },{
+    detail:
+      'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
+  },
+  {
     text: 'Absender: Authentisch',
-    detail: 'Authentische Absenderadressen sind z.B. Domains wie "bundesgerichtshof-karlsruhe.de" oder "bund-und-laender.online", wohingegen eine Domäne wie "why-how-what.info" weniger echt wirkt.'
-  },{
+    detail:
+      'Authentische Absenderadressen sind z.B. Domains wie "bundesgerichtshof-karlsruhe.de" oder "bund-und-laender.online", wohingegen eine Domäne wie "why-how-what.info" weniger echt wirkt.'
+  },
+  {
     text: 'Anrede: Personalisiert',
-    detail: 'In den Phishing Simulationen wird der Name des Empfängers in der Anrede verwendet, also z.B. "Sehr geehrter Herr Tim Kater".'
-  },{
+    detail:
+      'In den Phishing Simulationen wird der Name des Empfängers in der Anrede verwendet, also z.B. "Sehr geehrter Herr Tim Kater".'
+  },
+  {
     text: 'Unternehmensname',
-    detail: 'In den Phishing Simulationen wird Ihr Unternehmen referenziert, z.B. indem in einer Simulation eine "Kununu"-Bewertung zu Ihrer Firma nachgestellt wird.'
-  },{
+    detail:
+      'In den Phishing Simulationen wird Ihr Unternehmen referenziert, z.B. indem in einer Simulation eine "Kununu"-Bewertung zu Ihrer Firma nachgestellt wird.'
+  },
+  {
     text: '"Attack Pack": Behörden',
-    detail: 'Arbeitsrecht, Elternzeit, Unfallversicherung: Diese Inhalte sind für jeden interessant und haben eine hohe Klickquote.'
-  },{
+    detail:
+      'Arbeitsrecht, Elternzeit, Unfallversicherung: Diese Inhalte sind für jeden interessant und haben eine hohe Klickquote.'
+  },
+  {
     text: 'Abstimmung über neue Funktionen',
-    detail: 'Ihre Stimme zählt: Sie können regelmäßig über die Priorisierung von neuen Funktionen abstimmen.'
-  },{
+    detail:
+      'Ihre Stimme zählt: Sie können regelmäßig über die Priorisierung von neuen Funktionen abstimmen.'
+  },
+  {
     text: 'Abteilungsname',
-    detail: 'In den Phishing Simulationen wird die Abteilung des Empfängers referenziert, z.B. indem eine "Datenschutzbefragung" im "Einkauf" simuliert wird.'
-  },{
+    detail:
+      'In den Phishing Simulationen wird die Abteilung des Empfängers referenziert, z.B. indem eine "Datenschutzbefragung" im "Einkauf" simuliert wird.'
+  },
+  {
     text: 'Cyber-Verantwortlicher',
-    detail: 'In den Phishing Simulationen wird der Name des Cyber-Verantwortlichen angedruckt, z.B. dass "Reinhold Müller" via OneDrive eine Datei mit dem Empfänger geteilt hat.'
-  },{
+    detail:
+      'In den Phishing Simulationen wird der Name des Cyber-Verantwortlichen angedruckt, z.B. dass "Reinhold Müller" via OneDrive eine Datei mit dem Empfänger geteilt hat.'
+  },
+  {
     text: '"Attack Pack": Microsoft',
-    detail: 'Microsoft verschickt regelmäßig Nachrichten zu Produkten wie Teams und Outlook. In diesen Simulationen versenden wir entsprechende Kopien.'
+    detail:
+      'Microsoft verschickt regelmäßig Nachrichten zu Produkten wie Teams und Outlook. In diesen Simulationen versenden wir entsprechende Kopien.'
   }
 ];
 
 export const expertFeatures: ProductFeature[] = [
   {
     text: 'E-Mail-Support',
-    detail:'Unsere Reaktionszeiten sind in deutscher Sprache von Montag-Freitag zw. 9-16 Uhr und innerhalb von 48 Stunden.'
-  },{
+    detail:
+      'Unsere Reaktionszeiten sind in deutscher Sprache von Montag-Freitag zw. 9-16 Uhr und innerhalb von 48 Stunden.'
+  },
+  {
     text: 'Zahlweise: monatlich',
-    detail:'Auch bei Vertragslaufzeit länger als einen Monat erfolgt die Rechnungslegung monatlich, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den monatlichen Zahlungsterminen für die gesamte Vertragslaufzeit.'
-  },{
+    detail:
+      'Auch bei Vertragslaufzeit länger als einen Monat erfolgt die Rechnungslegung monatlich, d.h. Sie müssen nicht in Vorleistung gehen. Sie erhalten zu Vertragsbeginn eine Rechnung mit den monatlichen Zahlungsterminen für die gesamte Vertragslaufzeit.'
+  },
+  {
     text: 'Klickrate: Hoch',
-    detail:'Unsere Simulationen werden vorab immer von über 300 Anwendern in einer Testgruppe von unterschiedlichen Unternehmen einem Praxistest unterzogen: Dabei teilen wir die Klickquote in niedrig (>20%), mittel (>40%) und hoch (>60%) ein.'
-  },{
+    detail:
+      'Unsere Simulationen werden vorab immer von über 300 Anwendern in einer Testgruppe von unterschiedlichen Unternehmen einem Praxistest unterzogen: Dabei teilen wir die Klickquote in niedrig (>20%), mittel (>40%) und hoch (>60%) ein.'
+  },
+  {
     text: 'Anzahl E-Mails: 52',
     detail: 'Anzahl der Phishing Simulationen pro Empfänger und Jahr.'
-  },{
+  },
+  {
     text: 'Frequenz: 7 Tage',
-    detail: 'Jede Woche erhalten die Empfänger eine Phishing-Simulation. Damit erreichen Sie maximale Sensibilisierung ohne den Empfänger zu häufig zu stören.'
-  },{
+    detail:
+      'Jede Woche erhalten die Empfänger eine Phishing-Simulation. Damit erreichen Sie maximale Sensibilisierung ohne den Empfänger zu häufig zu stören.'
+  },
+  {
     text: 'Versandzeit: Zufällig',
-    detail: 'Jeder Empfänger erhält zu unterschiedlichen Zeiten unterschiedliche Phishing Simulationen.'
-  },{
+    detail:
+      'Jeder Empfänger erhält zu unterschiedlichen Zeiten unterschiedliche Phishing Simulationen.'
+  },
+  {
     text: 'Sprachniveau: Hoch',
-    detail: 'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
-  },{
+    detail:
+      'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausiblität geachtet.'
+  },
+  {
     text: 'Absender: Authentisch',
-    detail: 'Authentische Absenderadressen sind z.B. Domains wie "bundesgerichtshof-karlsruhe.de" oder "bund-und-laender.online", wohingegen eine Domäne wie "why-how-what.info" weniger echt wirkt.'
-  },{
+    detail:
+      'Authentische Absenderadressen sind z.B. Domains wie "bundesgerichtshof-karlsruhe.de" oder "bund-und-laender.online", wohingegen eine Domäne wie "why-how-what.info" weniger echt wirkt.'
+  },
+  {
     text: 'Anrede: Personalisiert',
-    detail: 'In den Phishing Simulationen wird der Name des Empfängers in der Anrede verwendet, also z.B. "Sehr geehrter Herr Tim Kater".'
-  },{
+    detail:
+      'In den Phishing Simulationen wird der Name des Empfängers in der Anrede verwendet, also z.B. "Sehr geehrter Herr Tim Kater".'
+  },
+  {
     text: 'Unternehmensname',
-    detail: 'In den Phishing Simulationen wird Ihr Unternehmen referenziert, z.B. indem in einer Simulation eine "Kununu"-Bewertung zu Ihrer Firma nachgestellt wird.'
-  },{
+    detail:
+      'In den Phishing Simulationen wird Ihr Unternehmen referenziert, z.B. indem in einer Simulation eine "Kununu"-Bewertung zu Ihrer Firma nachgestellt wird.'
+  },
+  {
     text: '"Attack Pack": Behörden',
-    detail: 'Arbeitsrecht, Elternzeit, Unfallversicherung: Diese Inhalte sind für jeden interessant und haben eine hohe Klickquote.'
-  },{
+    detail:
+      'Arbeitsrecht, Elternzeit, Unfallversicherung: Diese Inhalte sind für jeden interessant und haben eine hohe Klickquote.'
+  },
+  {
     text: 'Abstimmung über neue Funktionen',
-    detail: 'Ihre Stimme zählt: Sie können regelmäßig über die Priorisierung von neuen Funktionen abstimmen.'
-  },{
+    detail:
+      'Ihre Stimme zählt: Sie können regelmäßig über die Priorisierung von neuen Funktionen abstimmen.'
+  },
+  {
     text: 'Abteilungsname',
-    detail: 'In den Phishing Simulationen wird die Abteilung des Empfängers referenziert, z.B. indem eine "Datenschutzbefragung" im "Einkauf" simuliert wird.'
-  },{
+    detail:
+      'In den Phishing Simulationen wird die Abteilung des Empfängers referenziert, z.B. indem eine "Datenschutzbefragung" im "Einkauf" simuliert wird.'
+  },
+  {
     text: 'Cyber-Verantwortlicher',
-    detail: 'In den Phishing Simulationen wird der Name des Cyber-Verantwortlichen angedruckt, z.B. dass "Reinhold Müller" via OneDrive eine Datei mit dem Empfänger geteilt hat.'
-  },{
+    detail:
+      'In den Phishing Simulationen wird der Name des Cyber-Verantwortlichen angedruckt, z.B. dass "Reinhold Müller" via OneDrive eine Datei mit dem Empfänger geteilt hat.'
+  },
+  {
     text: '"Attack Pack": Microsoft',
-    detail: 'Microsoft verschickt regelmäßig Nachrichten zu Produkten wie Teams und Outlook. In diesen Simulationen versenden wir entsprechende Kopien.'
-  },{
+    detail:
+      'Microsoft verschickt regelmäßig Nachrichten zu Produkten wie Teams und Outlook. In diesen Simulationen versenden wir entsprechende Kopien.'
+  },
+  {
     text: '"Attack Pack": Banking',
-    detail: 'Speziell bei betrügerischen Zahlungsaufforderungen kann es schnell zu finanziellen Schäden kommen. Mit diesen Simulationen sollen Rechnungswesen und Einkauf gesondert sensibilisert werden.'
-  },{
+    detail:
+      'Speziell bei betrügerischen Zahlungsaufforderungen kann es schnell zu finanziellen Schäden kommen. Mit diesen Simulationen sollen Rechnungswesen und Einkauf gesondert sensibilisert werden.'
+  },
+  {
     text: 'Englischsprachige Texte',
-    detail: 'Einige Phishing Simulationen werden in englischer Sprache verfasst. Das hilft  Unternehmen, die regelmäßig mit dem Ausland korrespondieren.'
-  },{
+    detail:
+      'Einige Phishing Simulationen werden in englischer Sprache verfasst. Das hilft  Unternehmen, die regelmäßig mit dem Ausland korrespondieren.'
+  },
+  {
     text: 'Eigene Inhalte',
-    detail: 'Bei einer entsprechend großen Zahl an Empfänger können wir für Sie gezielt Inhalte einsteuern. Sprechen Sie uns darauf an.'
-  },{
+    detail:
+      'Bei einer entsprechend großen Zahl an Empfänger können wir für Sie gezielt Inhalte einsteuern. Sprechen Sie uns darauf an.'
+  },
+  {
     text: 'Funktionen vorschlagen',
     detail: 'Teilen Sie Ihre Ideen unserem Entwicklungsteam mit.'
-  },{
+  },
+  {
     text: 'Unternehmenszertifikat',
-    detail:'Nach 12 Monaten erhalten Sie ein auf Ihr Unternehmen ausgestelltes Zertifikat, das die kontinuierliche Sensibilisierung durch unsere Phishing Simulationen nachweist.'
-  },{
+    detail:
+      'Nach 12 Monaten erhalten Sie ein auf Ihr Unternehmen ausgestelltes Zertifikat, das die kontinuierliche Sensibilisierung durch unsere Phishing Simulationen nachweist.'
+  },
+  {
     text: 'Telefonischer Support',
-    detail:'Montag - Freitag zw. 9-16 Uhr mit Ausnahme gesetzlicher Feiertage in Baden-Württemberg.'
+    detail:
+      'Montag - Freitag zw. 9-16 Uhr mit Ausnahme gesetzlicher Feiertage in Baden-Württemberg.'
   }
 ];
