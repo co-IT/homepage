0a4f959f3adf63c082070828a9dd06eab88c883a
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2024-01-05 14:20:34 +0100

    feat(gesundheitswesen): add video thumbs

--- src/routes/cyber/cyber-portal/gesundheitswesen/resources/videos.ts
+++ src/routes/cyber/cyber-portal/gesundheitswesen/resources/videos.ts
@@ -4,16 +4,19 @@ export const anwenderVideos: YouTubeVideo[] = [
   {
     id: 'e6bEOffcyv0',
     title: 'Freigabe der Phishing Simulation in GMX',
-    thumb: '/img/cyber/cyber-portal/video-thumbnails/exchange-online.webp'
+    thumb:
+      '/img/cyber/cyber-portal/gesundheitswesen/wichtig-fuer-anwender/gmx.webp'
   },
   {
     id: 'NJRTV451uf0',
     title: 'Freigabe der Phishing Simulation in Gmail',
-    thumb: '/img/cyber/cyber-portal/video-thumbnails/exchange-2016.webp'
+    thumb:
+      '/img/cyber/cyber-portal/gesundheitswesen/wichtig-fuer-anwender/gmail.webp'
   },
   {
     id: 'G8SZjZ4_BtA',
     title: 'Freigabe der Phishing Simulation in web.de',
-    thumb: '/img/cyber/cyber-portal/video-thumbnails/sophos-xgs-mail.webp'
+    thumb:
+      '/img/cyber/cyber-portal/gesundheitswesen/wichtig-fuer-anwender/web.webp'
   }
 ];
