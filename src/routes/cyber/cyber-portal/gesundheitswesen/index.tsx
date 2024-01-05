07935ad50eb651b6565912d60b8cb3e83a5980c5
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2024-01-05 10:14:01 +0100

    feat(gesundheitswesen): remove pricing packages

--- src/routes/cyber/cyber-portal/gesundheitswesen/index.tsx
+++ src/routes/cyber/cyber-portal/gesundheitswesen/index.tsx
@@ -1,11 +1,4 @@
-import {
-  component$,
-  useComputed$,
-  useSignal,
-  useStore,
-  useStyles$,
-  useTask$
-} from '@builder.io/qwik';
+import { component$ } from '@builder.io/qwik';
 
 import { Section } from '~/components/section';
 
@@ -20,174 +13,43 @@ import {
 } from '~/components/heading-segment';
 import { LinkCallToAction } from '~/components/link-call-to-action';
 
-import { ShieldCheckmarkIcon } from '~/components/icons/shield-checkmark-icon';
-import { StarIcon } from '~/components/icons/star-icon';
-import { ThumbsUpIcon } from '~/components/icons/thumbs-up-icon';
 import { YouTubeVideoGrid } from '~/components/youtube';
 
 import {
   cyberVerantwortlicheVideos,
-  expertFeatures,
-  profiFeatures,
-  starterFeatures,
   wuerttembergischeFeatures
 } from './resources';
 
 import type { DocumentHead } from '@builder.io/qwik-city';
 import { LinkedHeading } from '~/components/link-heading';
-import { PricePerUsePerMonth } from '~/components/pricing/price-per-user-per-month';
 import { ProductFeatureList } from '~/components/product-feature-list';
-import style from './styles.css?inline';
 
 import PharmassecLogo from '~/media/gesundheitswesen/pharmassec_logo.png?jsx';
 
 export default component$(() => {
-  useStyles$(style);
-
-  const userCountSig = useSignal<string>('10');
-  const rangeValueSig = useSignal<string>('1');
-
-  const pricingTier = useStore({
-    starter: {
-      pricePerUserPerMonth: 0,
-      pricePerMonth: 0
-    },
-    professional: {
-      pricePerUserPerMonth: 0,
-      pricePerYearMonth: 0
-    },
-    expert: {
-      pricePerUserPerMonth: 0,
-      pricePerMonth: 0
-    }
-  });
-
-  const durationInMonthSig = useComputed$(() => {
-    const durationMonthMap = {
-      1: 1,
-      2: 3,
-      3: 6,
-      4: 12,
-      5: 18,
-      6: 24,
-      7: 36,
-      8: 48
-    } as const;
-
-    const range = rangeValueSig.value || 1;
-
-    //@ts-ignore
-    return durationMonthMap[range] as number;
-  });
-
-  const discountSig = useComputed$(() => {
-    const discountsAccordingToTerm = {
-      1: 0,
-      3: 0.02,
-      6: 0.04,
-      12: 0.1,
-      18: 0.12,
-      24: 0.15,
-      36: 0.18,
-      48: 0.2
-    } as const;
-
-    // @ts-ignore
-    return discountsAccordingToTerm[durationInMonthSig.value];
-  });
-
-  useTask$(({ track }) => {
-    const userCount = track(() => userCountSig.value);
-    const discount = track(() => discountSig.value);
-
-    if (userCount === undefined || discount === undefined) {
-      return;
-    }
-
-    updatePrices(+userCount);
-
-    function updatePrices(userCount: number) {
-      const basePrice = calculateBasePrice(userCount);
-
-      pricingTier.starter.pricePerUserPerMonth =
-        basePrice - basePrice * discount;
-
-      pricingTier.starter.pricePerMonth =
-        pricingTier.starter.pricePerUserPerMonth * userCount;
-
-      pricingTier.professional.pricePerUserPerMonth =
-        basePrice * 1.3 - basePrice * 1.3 * discount;
-
-      pricingTier.professional.pricePerYearMonth =
-        pricingTier.professional.pricePerUserPerMonth * userCount;
-
-      pricingTier.expert.pricePerUserPerMonth =
-        basePrice * 2 - basePrice * 2 * discount;
-
-      pricingTier.expert.pricePerMonth =
-        pricingTier.expert.pricePerUserPerMonth * userCount;
-    }
-
-    function calculateBasePrice(wantedUsers: number) {
-      wantedUsers = wantedUsers < 1 ? 1 : wantedUsers;
-      const pricingBronze = [
-        { user: 1, price: 20 },
-        { user: 5, price: 5 },
-        { user: 10, price: 3 },
-        { user: 25, price: 2.75 },
-        { user: 50, price: 2.5 },
-        { user: 75, price: 1.5 },
-        { user: 100, price: 1 },
-        { user: 200, price: 0.93 },
-        { user: 250, price: 0.84 },
-        { user: 500, price: 0.6 },
-        { user: 750, price: 0.5 },
-        { user: 1000, price: 0.4 },
-        { user: 2000, price: 0.38 },
-        { user: 3000, price: 0.36 },
-        { user: 4000, price: 0.34 },
-        { user: 5000, price: 0.32 },
-        { user: 6000, price: 0.3 }
-      ];
-      const lastElement = pricingBronze[pricingBronze.length - 1];
-      if (wantedUsers > lastElement.user) return lastElement.price;
-
-      let lastBigger = pricingBronze[0];
-      let lastSmaller = pricingBronze[0];
-
-      for (let i = 1; i < pricingBronze.length; i++) {
-        lastBigger = pricingBronze[i];
-        if (pricingBronze[i].user > wantedUsers) break;
-        lastSmaller = pricingBronze[i];
-      }
-
-      const price = calculatePointOnLine(
-        lastSmaller.user,
-        lastSmaller.price,
-        lastBigger.user,
-        lastBigger.price,
-        wantedUsers
-      );
-
-      return price;
-    }
-
-    function calculatePointOnLine(
-      x1: number,
-      y1: number,
-      x2: number,
-      y2: number,
-      x: number
-    ) {
-      let slope = (y2 - y1) / (x2 - x1);
-      if (isNaN(slope)) slope = 0;
-      const yIntercept = y1 - slope * x1;
-      return slope * x + yIntercept;
-    }
-  });
-
   return (
     <>
+      <Section id='besonderheiten'>
+        <ContentOverlapPortrait
+          image={{
+            source: '/img/cyber/cyber-portal/gesundheitswesen/Antrieb.webp',
+            alternateText: 'Besonderheiten im Gesundheitswesen'
+          }}
+        >
+          <section q:slot='article' class='flex flex-col space-y-4'>
+            <LinkedHeading href='#unser_antrieb'>
+              <HeadingSegmentSecondary text='Besunderheiten im' />
+              &nbsp;
+              <HeadingSegmentSecondary800 text='Gesundheitswesen' />
+            </LinkedHeading>
+            <p class='pb-4 leading-7'>
+              Kritische Infrastruktur, Besonders sensible Daten, Kein
+              klassisches Büro.
+            </p>
+          </section>
+        </ContentOverlapPortrait>
+      </Section>
+
       <Section id='unser_antrieb'>
         <ContentOverlapPortrait
           image={{
@@ -353,151 +215,6 @@ export default component$(() => {
             </LinkCallToAction>
           </section>
         </ContentOverlapPortrait>
-      </Section>
-
-      <Section id='lust_auf_mehr' backgroundColor='gray'>
-        <section class='grid gap-8 px-4'>
-          <LinkedHeading href='#lust_auf_mehr'>
-            Lust auf <HeadingSegmentSecondary800 text='mehr?' />
-          </LinkedHeading>
-
-          <p class='leading-7'>
-            Hier finden Sie alle Informationen zu den erweiterten Paketen auf
-            einen Blick. <br /> Der Konfigurator unterstützt Sie bei der Auswahl
-            des passenden Pakets.
-          </p>
-
-          <div class='flex flex-wrap items-center gap-4 rounded-3xl'>
-            <section class='flex flex-wrap gap-4'>
-              <span class='text-xl font-bold'>Anzahl Empfänger</span>
-              <input
-                type='number'
-                bind:value={userCountSig}
-                onKeyUp$={() => {
-                  if (+userCountSig.value < 0) {
-                    userCountSig.value = '1';
-                  }
-                }}
-                required
-                value='10'
-                min='1'
-                max='6000'
-                class='border-b border-secondary-900 text-center'
-              />
-            </section>
-            <section class='flex flex-wrap items-center gap-4'>
-              <span class='text-xl font-bold'>Laufzeit</span>
-              <input
-                type='range'
-                bind:value={rangeValueSig}
-                required
-                min='1'
-                max='8'
-                step='1'
-              />
-              {durationInMonthSig.value === 1 && (
-                <small class='min-w-[4rem]'>
-                  {durationInMonthSig.value} Monat
-                </small>
-              )}
-              {durationInMonthSig.value > 1 && (
-                <small class='min-w-[4rem]'>
-                  {durationInMonthSig.value} Monate
-                </small>
-              )}
-
-              <span
-                class={`${
-                  discountSig.value === 0 ? 'opacity-50' : ''
-                } grid w-32 items-center rounded bg-secondary-900 p-2 text-center text-xs font-medium text-primary`}
-              >
-                {discountSig.value * 100}% Rabatt
-              </span>
-            </section>
-          </div>
-
-          <div class='pricing-tiers'>
-            {/* Starter */}
-            <div class='card grid items-start gap-8 shadow-md'>
-              <div class='features'>
-                <h3 class='mb-8 flex items-center gap-4 text-3xl font-bold'>
-                  <ThumbsUpIcon />
-                  Starter
-                </h3>
-                <ul>
-                  <ProductFeatureList features={starterFeatures} />
-                </ul>
-              </div>
-
-              <div class='grid gap-8 self-end'>
-                <PricePerUsePerMonth
-                  usersCount={+userCountSig.value}
-                  pricePerUser={pricingTier.starter.pricePerUserPerMonth}
-                />
-                <LinkCallToAction
-                  href='https://forms.office.com/e/x9y2sJSm8P'
-                  target='_blank'
-                  class='mx-auto'
-                >
-                  Anfordern
-                </LinkCallToAction>
-              </div>
-            </div>
-
-            {/* Profi */}
-            <div class='card grid items-start gap-8 shadow-xl'>
-              <div class='features'>
-                <h3 class='mb-8 flex items-center gap-4 text-3xl font-bold'>
-                  <StarIcon />
-                  Profi
-                </h3>
-                <ul>
-                  <ProductFeatureList features={profiFeatures} />
-                </ul>
-              </div>
-
-              <div class='grid gap-8 self-end'>
-                <PricePerUsePerMonth
-                  usersCount={+userCountSig.value}
-                  pricePerUser={pricingTier.professional.pricePerUserPerMonth}
-                />
-                <LinkCallToAction
-                  href='https://forms.office.com/e/x9y2sJSm8P'
-                  target='_blank'
-                  class='mx-auto'
-                >
-                  Anfordern
-                </LinkCallToAction>
-              </div>
-            </div>
-
-            {/* Expert */}
-            <div class='card grid items-start gap-8 shadow-md' id='gold'>
-              <div class='features'>
-                <h3 class='mb-8 flex items-center gap-4 text-3xl font-bold'>
-                  <ShieldCheckmarkIcon />
-                  Experte
-                </h3>
-                <ul>
-                  <ProductFeatureList features={expertFeatures} />
-                </ul>
-              </div>
-              <div class='grid gap-8 self-end'>
-                <PricePerUsePerMonth
-                  usersCount={+userCountSig.value}
-                  pricePerUser={pricingTier.expert.pricePerUserPerMonth}
-                />
-                <LinkCallToAction
-                  href='https://forms.office.com/e/x9y2sJSm8P'
-                  target='_blank'
-                  class='mx-auto'
-                >
-                  Anfordern
-                </LinkCallToAction>
-              </div>
-            </div>
-          </div>
-        </section>
       </Section>
 
       <Section id='fordern_sie_ihr_angebot_an'>
