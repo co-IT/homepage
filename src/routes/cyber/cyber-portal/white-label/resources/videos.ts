b9903fa8b6949e9ea203e6be07f8a27231d911ce
Uli Armbruster <uli.armbruster@co-IT.eu>
2024-01-07 13:30:28 +0100

    feat(cyber-portal/white-label): Fünte Iteration

--- src/routes/cyber/cyber-portal/white-label/resources/videos.ts
+++ src/routes/cyber/cyber-portal/white-label/resources/videos.ts
@@ -2,27 +2,39 @@ import type { YouTubeVideo } from '~/components/youtube';
 
 export const anwenderVideos: YouTubeVideo[] = [
   {
-    id: 'KSSDqIIikvQ',
-    title: 'Cyber Portal Württembergische: Funktionalitäten der Anwender',
-    thumb:
-      '/img/cyber/cyber-portal/white-label/cyber_portal_anwender.webp'
-  },
-  {
-    id: '_SZyfQB7n7k',
-    title: 'Cyber Portal Württembergische: Funktionalitäten für Entscheider',
-    thumb:
-      '/img/cyber/cyber-portal/white-label/cyber_portal_verantwortliche.webp'
-  },
-  {
     id: 'Qsjrg9Mj5QQ',
     title: 'Kurs: Effektives Basiswissen',
     thumb:
-      '/img/cyber/cyber-portal/white-label/Kurs_Effektives_Basiswissen.webp'
+      '/img/cyber/cyber-portal/white-label/Kurs1.webp'
   },
   {
     id: 'LrjckqfadeE',
     title: 'Kurs: E-Mail-Sicherheit',
     thumb:
-      '/img/cyber/cyber-portal/white-label/Kurs_E-Mail-Sicherheit.webp'
+      '/img/cyber/cyber-portal/white-label/Kurs2.webp'
+  },
+  {
+    id: 'Apl91xmNOJs',
+    title: 'Kurs: Sichere Passwörter',
+    thumb:
+      '/img/cyber/cyber-portal/white-label/Kurs3.webp'
+  },
+  {
+    id: 'pwhDfa5ICZg',
+    title: 'Kurs: Sicher surfen',
+    thumb:
+      '/img/cyber/cyber-portal/white-label/Kurs4.webp'
+  },
+  {
+    id: 'pB8yRwUFxW4',
+    title: 'Kurs: Onlinebanking',
+    thumb:
+      '/img/cyber/cyber-portal/white-label/Kurs5.webp'
+  },
+  {
+    id: 'qvl-qM38j6M',
+    title: 'Kurs: Datenschutz',
+    thumb:
+      '/img/cyber/cyber-portal/white-label/Kurs6.webp'
   }
 ];
