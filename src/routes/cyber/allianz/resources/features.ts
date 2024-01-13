65be7ae8f2f4a04870352ac83a91f4e5969e2e0d
Gregor Woiwode <gregor.woiwode@gmail.com>
2024-01-13 21:21:45 +0100

    feat(allianz): bootstrap page

---
+++ src/routes/cyber/allianz/resources/features.ts
@@ -0,0 +1,48 @@
+import type { ProductFeature } from '~/components/product-feature-list';
+
+export const ihreMarkeFeatures: ProductFeature[] = [
+  {
+    text: 'Anzahl Simulationen: Beliebig',
+    detail:
+      'Es handelt sich um die übers Jahr insgesamt verschickten Phishing-Simulationen. Bei einer geringeren Vertragslaufzeit erhalten Sie pro Monat der Vertragslaufzeit jeweils 1/12 der Phishing-Mails.'
+  },
+  {
+    text: 'Ihre Branche',
+    detail: 'Sprechen Sie uns an, was Sie sich für Ihre Kunden wünschen.'
+  },
+  {
+    text: 'Ihre Inhalte',
+    detail:
+      'Wir gestalten mit Ihnen gemeinsam genau die Inhalte, die für Apotheken, Sanitärshäuser und Arztpraxen relevant sind, damit diese auf die gefährlichsten Phishing-Angriffe vorbereitet sind.'
+  },
+  {
+    text: 'Sprachniveau: Hoch',
+    detail:
+      'Das Sprachniveau variiert zw. niedrig und hoch. Bei mittlerem Niveau wird korrekte Rechtschreibung und Grammatik benutzt. Auf hohem Sprachniveau wird zusätzlich auf eine klare Semantik und Plausibilität geachtet.'
+  },
+  {
+    text: 'Randomisierung: überraschend',
+    detail:
+      'Alle Anwender erhalten unterschiedliche Simulationen zu unterschiedlichen Zeiten.'
+  },
+  {
+    text: 'Absender: Authentisch',
+    detail:
+      'Authentische Absenderadressen sind z.B. Domains wie "ratiopharm-info.de" oder "apotheker-netzwerk.de", wohingegen eine Domäne wie "why-how-what.info" weniger echt wirkt.'
+  },
+  {
+    text: 'Anrede: Personalisiert',
+    detail:
+      'In den Phishing Simulationen wird der Name des Empfängers in der Anrede verwendet, also z.B. "Sehr geehrter Herr Tim Kater".'
+  },
+  {
+    text: 'Unternehmensname',
+    detail:
+      'In den Phishing Simulationen wird Ihr Unternehmen referenziert, z.B. indem in einer Simulation eine "Jameda"-Bewertung zu Ihrer Firma nachgestellt wird.'
+  },
+  {
+    text: 'Unternehmenszertifikat',
+    detail:
+      'Nach 12 Monaten erhalten Sie ein auf Ihr Unternehmen ausgestelltes Zertifikat, das die kontinuierliche Sensibilisierung durch unsere Phishing Simulationen nachweist.'
+  }
+];
