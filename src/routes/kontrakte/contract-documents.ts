7197fb7bae64f18fc0e5830f76e2c6b66120f8e4
Frank Strieter <frank.strieter@co-IT.eu>
2025-07-02 12:53:09 +0200

    fix: order in kontrakte

--- src/routes/kontrakte/contract-documents.ts
+++ src/routes/kontrakte/contract-documents.ts
@@ -9,16 +9,16 @@ export const contractDocumentsByCategory: ContractDocumentsByCategory = [
         url: 'https://dl.co-it.eu/kontrakte/AGB_v2.0.pdf'
       },
       {
-        name: 'Datenschutzerklärung',
-        version: '2.1',
-        createdAt: '01.07.2025',
-        url: 'https://dl.co-it.eu/kontrakte/Website_Datenschutz_v2.1.pdf'
-      },
-      {
         name: 'AGB',
         version: '1.0',
         createdAt: '04.05.2022',
         url: 'https://dl.co-it.eu/kontrakte/AGB_v1.0.pdf'
+      },
+      {
+        name: 'Datenschutzerklärung',
+        version: '2.1',
+        createdAt: '01.07.2025',
+        url: 'https://dl.co-it.eu/kontrakte/Website_Datenschutz_v2.1.pdf'
       },
       {
         name: 'Datenschutzerklärung',
