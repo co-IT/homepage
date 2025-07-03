51bfd5cecbbc35221817bd39f659e6a5cfd6947d
Frank Strieter <frank.strieter@co-IT.eu>
2025-07-03 08:18:41 +0200

    fix: remove blue frame on team picture

--- src/components/team-section/team-section.tsx
+++ src/components/team-section/team-section.tsx
@@ -2,7 +2,7 @@ import { component$ } from '@builder.io/qwik';
 import { LinkCallToAction } from '../link-call-to-action';
 export const TeamSection = component$(() => {
   return (
-    <div class='relative flex flex-col bg-secondary-900'>
+    <div class='relative flex flex-col '>
       <div class='absolute left-0 right-0 top-10 z-10'>
         <div class='leading-1 mx-auto max-w-5xl px-4 text-center font-normal text-white sm:text-base md:px-14 md:text-2xl lg:leading-10'>
           "Unsere Motivation ist es eine <span class='font-semibold text-primary'>aufgeschlossene Welt</span> zu
@@ -22,16 +22,17 @@ export const TeamSection = component$(() => {
           </LinkCallToAction>
         </div>
       </div>
-
-      <img
-        class='mx-auto min-h-[350px]'
-        loading='lazy'
-        src='/img/about/team.webp'
-        width='1920'
-        height='601'
-        alt='Picture showing the whole team at co-IT'
-        style='mask-image: linear-gradient(0deg, rgba(2, 2, 52, 0.6), rgba(2, 2, 52, 0.6)); object-fit: cover;'
-      />
+      <div class='mx-auto max-w-[1920px] bg-secondary-900'>
+        <img
+          class='min-h-[350px]'
+          loading='lazy'
+          src='/img/about/team.webp'
+          width='1920'
+          height='601'
+          alt='Picture showing the whole team at co-IT'
+          style='mask-image: linear-gradient(0deg, rgba(2, 2, 52, 0.6), rgba(2, 2, 52, 0.6)); object-fit: cover;'
+        />
+      </div>
     </div>
   );
 });
